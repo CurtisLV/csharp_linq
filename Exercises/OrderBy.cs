a305849e0c5e399693a35af8b0717b203b79d229
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-02-23 18:18:38 +0200

    23.02. 2

--- Exercises/OrderBy.cs
+++ Exercises/OrderBy.cs
@@ -8,13 +8,12 @@ namespace Exercises
     {
         //Coding Exercise 1
         /*
-         Using LINQ, implement the OrderFromLongestToShortest method, which takes 
-        a collection of strings, and returns those strings ordered from 
+         Using LINQ, implement the OrderFromLongestToShortest method, which takes
+        a collection of strings, and returns those strings ordered from
         longest to shortest.
         For example, for {"bb", "a", "ccc"} the result should be {"ccc", "bb", "a"}
          */
-        public static IEnumerable<string> OrderFromLongestToShortest(
-            IEnumerable<string> words)
+        public static IEnumerable<string> OrderFromLongestToShortest(IEnumerable<string> words)
         {
             //TODO your code goes here
             throw new NotImplementedException();
@@ -22,15 +21,14 @@ namespace Exercises
 
         //Coding Exercise 2
         /*
-         Using LINQ, implement the FirstEvenThenOddDescending method, 
+         Using LINQ, implement the FirstEvenThenOddDescending method,
          which orders numbers like this:
             *first, the even numbers
             *then, the odd numbers
-         Then the numbers should be ordered descending. 
+         Then the numbers should be ordered descending.
          For example, for numbers {1,2,3,4,5,6,7} the result should be: {6,4,2,7,5,3,1}.
          */
-        public static IEnumerable<int> FirstEvenThenOddDescending(
-            IEnumerable<int> numbers)
+        public static IEnumerable<int> FirstEvenThenOddDescending(IEnumerable<int> numbers)
         {
             //TODO your code goes here
             throw new NotImplementedException();
@@ -47,10 +45,12 @@ namespace Exercises
         //do not modify this method
         public static IEnumerable<DateTime> OrderByMonth(List<DateTime> dates)
         {
-            dates.Sort((left, right) =>
-            {
-                return left.Month.CompareTo(right.Month);
-            });
+            dates.Sort(
+                (left, right) =>
+                {
+                    return left.Month.CompareTo(right.Month);
+                }
+            );
             return dates;
         }
     }
