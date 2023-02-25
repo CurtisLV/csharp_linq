434d23d98d43e94ff228528dcd8cd3ea761b9d8e
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-02-25 16:01:22 +0200

    25.02. 11

--- ExercisesTests/Average_Exercise2_Tests.cs
+++ ExercisesTests/Average_Exercise2_Tests.cs
@@ -12,18 +12,21 @@ namespace ExercisesTests
         [Test]
         public void EmptyStudents()
         {
-            var students = new List<Student>
-            {
-
-            };
+            var students = new List<Student> { };
             try
             {
                 var result = Average.MaxAverageOfMarks(students);
-                Assert.AreEqual(0, result, $"For empty collection of students, the result shall be 0");
+                Assert.AreEqual(
+                    0,
+                    result,
+                    $"For empty collection of students, the result shall be 0"
+                );
             }
             catch (Exception ex)
             {
-                Assert.Fail($"For empty collection of students, the result shall be 0. Exception message: {ex.Message}");
+                Assert.Fail(
+                    $"For empty collection of students, the result shall be 0. Exception message: {ex.Message}"
+                );
             }
         }
 
@@ -32,18 +35,34 @@ namespace ExercisesTests
         {
             var students = new List<Student>
             {
-                new Student {Marks = new List<int> {4,5,5}},
-                new Student {Marks = new List<int> {5,6,5}},
-                new Student {Marks = new List<int> {4,4,5}},
+                new Student
+                {
+                    Marks = new List<int> { 4, 5, 5 }
+                },
+                new Student
+                {
+                    Marks = new List<int> { 5, 6, 5 }
+                },
+                new Student
+                {
+                    Marks = new List<int> { 4, 4, 5 }
+                },
             };
             try
             {
                 var result = Average.MaxAverageOfMarks(students);
-                Assert.AreEqual(5.333, result, 0.1, $"For students with marks (4,5,5), (5,6,5), (4,4,5) max average mark is 5.33");
+                Assert.AreEqual(
+                    5.333,
+                    result,
+                    0.1,
+                    $"For students with marks (4,5,5), (5,6,5), (4,4,5) max average mark is 5.33"
+                );
             }
             catch (Exception ex)
             {
-                Assert.Fail($"For students with marks (4,5,5), (5,6,5), (4,4,5) max average mark is 5.33. Exception message: {ex.Message}");
+                Assert.Fail(
+                    $"For students with marks (4,5,5), (5,6,5), (4,4,5) max average mark is 5.33. Exception message: {ex.Message}"
+                );
             }
         }
 
@@ -52,16 +71,56 @@ namespace ExercisesTests
         {
             var students = new List<Student>
             {
-                new Student {Marks = new List<int> {}},
+                new Student
+                {
+                    Marks = new List<int> { 4, 5, 5 }
+                },
+                new Student
+                {
+                    Marks = new List<int> { 5, 6, 5 }
+                },
+                new Student
+                {
+                    Marks = new List<int> { 4, 4, 5 }
+                },
             };
             try
             {
                 var result = Average.MaxAverageOfMarks(students);
-                Assert.AreEqual(0, result, 0.1, $"If no student has any marks, the result shall be 0.");
+                Assert.AreEqual(
+                    5.333,
+                    result,
+                    0.1,
+                    $"For students with marks (4,5,5), (5,6,5), (4,4,5) max average mark is 5.33"
+                );
             }
             catch (Exception ex)
             {
-                Assert.Fail($"If no student has any marks, the result shall be 0. Exception message: {ex.Message}");
+                Assert.Fail(
+                    $"For students with marks (4,5,5), (5,6,5), (4,4,5) max average mark is 5.33. Exception message: {ex.Message}"
+                );
+            }
+        }
+
+        [Test]
+        public void NonEmpty_NoMarksAtAll()
+        {
+            var students = new List<Student> { new Student { Marks = new List<int> { } }, };
+            try
+            {
+                var result = Average.MaxAverageOfMarks(students);
+                Assert.AreEqual(
+                    0,
+                    result,
+                    0.1,
+                    $"If no student has any marks, the result shall be 0."
+                );
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(
+                    $"If no student has any marks, the result shall be 0. Exception message: {ex.Message}"
+                );
             }
         }
     }
