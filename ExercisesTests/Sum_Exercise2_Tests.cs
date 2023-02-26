fd2eccd68260f82ff002322dbe38da0e8485ff25
CurtisLV <curtislv@gmail.com>
2023-02-26 18:21:58 +0200

    26.02. 8

--- ExercisesTests/Sum_Exercise2_Tests.cs
+++ ExercisesTests/Sum_Exercise2_Tests.cs
@@ -11,16 +11,19 @@ namespace ExercisesTests
         public void AverageSumShallWorkCorrectly()
         {
             var input = new List<List<int>>
-           {
-               new List<int> {1,2,3,2},
-               new List<int> {1,5,6},
-               new List<int> {2,2},
-           };
+            {
+                new List<int> { 1, 2, 3, 2 },
+                new List<int> { 1, 5, 6 },
+                new List<int> { 2, 2 },
+            };
             var result = Sum.AverageSum(input);
             var expectedResult = 8;
-            Assert.AreEqual(expectedResult, result, 
-                $"For INPUT ((1,2,3,2),(1,5,6),(2,2)) the RESULT shall be " +
-                $"{expectedResult} but IT WAS {result}");
+            Assert.AreEqual(
+                expectedResult,
+                result,
+                $"For INPUT ((1,2,3,2),(1,5,6),(2,2)) the RESULT shall be "
+                    + $"{expectedResult} but IT WAS {result}"
+            );
         }
     }
 }
