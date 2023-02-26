1b0619a91fdcff82317576cdffc2186b4b083c00
CurtisLV <curtislv@gmail.com>
2023-02-26 18:03:55 +0200

    26.02. 1

--- Exercises/Sum.cs
+++ Exercises/Sum.cs
@@ -8,9 +8,9 @@ namespace Exercises
     {
         //Coding Exercise 1
         /*
-         Implement the TotalLength method, which given a collection of words will 
+         Implement the TotalLength method, which given a collection of words will
          calculate the sum of their lengths.
-         For example, for words {"little", "brown", "fox"} the result shall be 
+         For example, for words {"little", "brown", "fox"} the result shall be
          14 because the lengths of those words are 6, 5, and 3, which sums up to 14.
          Assume the result is not null.
          */
@@ -22,21 +22,20 @@ namespace Exercises
 
         //Coding Exercise 2
         /*
-         Implement the AverageSum method, which given a nested collection of numbers 
-        (so a collection of collections of numbers) will return the average sum 
+         Implement the AverageSum method, which given a nested collection of numbers
+        (so a collection of collections of numbers) will return the average sum
         for those collections.
         For example, for the following input...
         {
-            {1,2,3,2}, //sum is 8
-            {1,5,6}, //sum is 12
-            {2,2} //sum is 4
+{1,2,3,2}, //sum is 8
+{1,5,6}, //sum is 12
+{2,2} //sum is 4
         }
         ...the result shall be 8 because the average sum is (8+12+4)/3 = 8.
 
         Assume the result is not null and non-empty.
          */
-        public static double AverageSum(
-            IEnumerable<IEnumerable<int>> collectionsOfNumbers)
+        public static double AverageSum(IEnumerable<IEnumerable<int>> collectionsOfNumbers)
         {
             //TODO your code goes here
             throw new NotImplementedException();
@@ -45,15 +44,15 @@ namespace Exercises
         //Refactoring challenge
         //TODO implement this method
         public static bool HasAnyStudentSumOfMarksLargerThan100_Refactored(
-            IEnumerable<Student> students)
+            IEnumerable<Student> students
+        )
         {
             //TODO your code goes here
             throw new NotImplementedException();
         }
 
         //do not modify this method
-        public static bool HasAnyStudentSumOfMarksLargerThan100(
-            IEnumerable<Student> students)
+        public static bool HasAnyStudentSumOfMarksLargerThan100(IEnumerable<Student> students)
         {
             foreach (var student in students)
             {
