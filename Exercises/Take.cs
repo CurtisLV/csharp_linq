52bc52565dbbff7755231fc02e33e0c945d11931
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-03-03 17:56:40 +0200

    03.03. 11

--- Exercises/Take.cs
+++ Exercises/Take.cs
@@ -49,7 +49,7 @@ namespace Exercises
             int percent
         )
         {
-            return pets.OrderByDescending(pet => pet.Weight).Take(pets.Count() * percent / 100);
+            return pets.OrderBy(pet => pet.Weight).TakeLast(pets.Count() * percent / 100);
         }
 
         //Refactoring challenge
