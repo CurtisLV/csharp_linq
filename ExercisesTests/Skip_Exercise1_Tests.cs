96d6334256dedb74dfae4570bd4b840c24bdbc36
CurtisLV <curtislv@gmail.com>
2023-03-04 17:43:00 +0200

    04.03. 2

--- ExercisesTests/Skip_Exercise1_Tests.cs
+++ ExercisesTests/Skip_Exercise1_Tests.cs
@@ -10,10 +10,7 @@ namespace ExercisesTests
         [Test]
         public void CalculateAverageMark()
         {
-            var student = new Student
-            {
-                Marks = new int[] { 3, 4, 6, 6, 2, 5 }
-            };
+            var student = new Student { Marks = new int[] { 3, 4, 6, 6, 2, 5 } };
             var result = Skip.CalculateAverageMark(student);
             Assert.AreEqual(4.5, result, $"For student with marks 3,4,6,6,2,5 result shall be 4.5");
         }
@@ -21,10 +18,7 @@ namespace ExercisesTests
         [Test]
         public void CalculateAverageMark_EmptyMarks()
         {
-            var student = new Student
-            {
-                Marks = new int[] { }
-            };
+            var student = new Student { Marks = new int[] { } };
             var result = Skip.CalculateAverageMark(student);
             Assert.AreEqual(0, result, $"For student with no marks result shall be 0");
         }
@@ -32,10 +26,7 @@ namespace ExercisesTests
         [Test]
         public void CalculateAverageMark_LessThan3Marks()
         {
-            var student = new Student
-            {
-                Marks = new int[] { 2, 5 }
-            };
+            var student = new Student { Marks = new int[] { 2, 5 } };
             var result = Skip.CalculateAverageMark(student);
             Assert.AreEqual(0, result, $"For student with less than 3 marks result shall be 0");
         }
