edbac9c1e79abbefad4dacfd0794a56204c978ad
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-03-06 17:46:34 +0200

    06.03. 1

--- Exercises/Distinct.cs
+++ Exercises/Distinct.cs
@@ -8,12 +8,12 @@ namespace Exercises
     {
         //Coding Exercise 1
         /*
-         Implement the AreAllUnique method, which will check if all elements of 
+         Implement the AreAllUnique method, which will check if all elements of
         a collection are unique.
 
         For example:
             *for numbers 1,2,3,4 the result shall be true because no number is duplicated
-            *for strings AAA, BBB, BBB, CCC the result shall be false 
+            *for strings AAA, BBB, BBB, CCC the result shall be false
                 because BBB is duplicated
          */
         public static bool AreAllUnique<T>(IEnumerable<T> collection)
@@ -24,25 +24,26 @@ namespace Exercises
 
         //Coding Exercise 2
         /*
-         Implement the GetCollectionWithMostDuplicates method, which given 
-        a collection of collections will return the collection with 
-        the most duplicates in it. 
-        If a couple of collections have the same count of duplicates, 
-        the shortest should be returned. 
+         Implement the GetCollectionWithMostDuplicates method, which given
+        a collection of collections will return the collection with
+        the most duplicates in it.
+        If a couple of collections have the same count of duplicates,
+        the shortest should be returned.
         If the collections parameter is empty, the result shall be null.
 
-        Let's consider the following collections:        
-            *{1,2,3,4} - it has 0 duplicates        
-            *{1,2,3,3,4,4,4} - it has 3 duplicates: 
-                one 3 is a duplicate, and two 4s are duplicates       
-            *{1,2,3,3,4,4,4,5,6,7} - it has 3 duplicates: one 3 is a duplicate, 
+        Let's consider the following collections:
+            *{1,2,3,4} - it has 0 duplicates
+            *{1,2,3,3,4,4,4} - it has 3 duplicates:
+                one 3 is a duplicate, and two 4s are duplicates
+            *{1,2,3,3,4,4,4,5,6,7} - it has 3 duplicates: one 3 is a duplicate,
                 and two 4s are duplicates
         
-        The result shall be the second collection, because it has the most duplicates, 
+        The result shall be the second collection, because it has the most duplicates,
         and it is the shortest of two collections with 3 duplicates
          */
         public static IEnumerable<T> GetCollectionWithMostDuplicates<T>(
-            IEnumerable<IEnumerable<T>> collections)
+            IEnumerable<IEnumerable<T>> collections
+        )
         {
             //TODO your code goes here
             throw new NotImplementedException();
@@ -51,15 +52,15 @@ namespace Exercises
         //Refactoring challenge
         //TODO implement this method
         public static IEnumerable<string> GetWordsShorterThan5Letters_Refactored(
-            IEnumerable<string> words)
+            IEnumerable<string> words
+        )
         {
             //TODO your code goes here
             throw new NotImplementedException();
         }
 
         //do not modify this method
-        public static IEnumerable<string> GetWordsShorterThan5Letters(
-            IEnumerable<string> words)
+        public static IEnumerable<string> GetWordsShorterThan5Letters(IEnumerable<string> words)
         {
             var result = new List<string>();
             foreach (var word in words)
