ad634f19d5bc90cc58cc19c8e81a65c06ac8afb5
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-03-06 17:52:24 +0200

    06.03. 2

--- ExercisesTests/Distinct_Exercise2_Tests.cs
+++ ExercisesTests/Distinct_Exercise2_Tests.cs
@@ -13,15 +13,19 @@ namespace ExercisesTests
         {
             var collections = new List<List<int>>
             {
-              new List<int> {1,2,3,4},
-              new List<int> {1,2,3,3,4,4,4},
-              new List<int> {1,2,3,3,4,4,4,5,6,7},
+                new List<int> { 1, 2, 3, 4 },
+                new List<int> { 1, 2, 3, 3, 4, 4, 4 },
+                new List<int> { 1, 2, 3, 3, 4, 4, 4, 5, 6, 7 },
             };
 
             var expectedResult = collections[1];
             var result = Distinct.GetCollectionWithMostDuplicates(collections);
             var resultAsString = string.Join(",", result);
-            CollectionAssert.AreEqual(expectedResult, result, $"For input collections (1,2,3,4), (1,2,3,3,4,4,4), (1,2,3,3,4,4,4,5,6,7) the result collection shall be (1,2,3,3,4,4,4), but it was ({resultAsString})");
+            CollectionAssert.AreEqual(
+                expectedResult,
+                result,
+                $"For input collections (1,2,3,4), (1,2,3,3,4,4,4), (1,2,3,3,4,4,4,5,6,7) the result collection shall be (1,2,3,3,4,4,4), but it was ({resultAsString})"
+            );
         }
 
         [Test]
@@ -29,17 +33,21 @@ namespace ExercisesTests
         {
             var collections = new List<List<int>>
             {
-              new List<int> {1,2,3,3,4,4,4},
-              new List<int> {1,2,3,4},
-              new List<int> {1,2,3,3,4,4,4,5,6,7},
-              new List<int> {1,2,3,4,4,4,5,6,7},
+                new List<int> { 1, 2, 3, 3, 4, 4, 4 },
+                new List<int> { 1, 2, 3, 4 },
+                new List<int> { 1, 2, 3, 3, 4, 4, 4, 5, 6, 7 },
+                new List<int> { 1, 2, 3, 4, 4, 4, 5, 6, 7 },
             };
 
             var expectedResult = collections[0];
             var result = Distinct.GetCollectionWithMostDuplicates(collections);
 
             var resultAsString = string.Join(",", result);
-            CollectionAssert.AreEqual(expectedResult, result, $"For input collections (1,2,3,3,4,4,4), (1,2,3,4), (1,2,3,3,4,4,4,5,6,7), (1,2,3,4,4,4,5,6,7) the result collection shall be (1,2,3,3,4,4,4), but it was ({resultAsString})");
+            CollectionAssert.AreEqual(
+                expectedResult,
+                result,
+                $"For input collections (1,2,3,3,4,4,4), (1,2,3,4), (1,2,3,3,4,4,4,5,6,7), (1,2,3,4,4,4,5,6,7) the result collection shall be (1,2,3,3,4,4,4), but it was ({resultAsString})"
+            );
         }
 
         [Test]
@@ -47,17 +55,21 @@ namespace ExercisesTests
         {
             try
             {
-                var collections = new List<List<int>>
-                {
-                };
+                var collections = new List<List<int>> { };
 
                 var result = Distinct.GetCollectionWithMostDuplicates(collections);
                 var resultAsString = result == null ? "null" : string.Join(",", result);
-                CollectionAssert.AreEqual(null, result, $"For an empty input collection the result shall be null, but it was ({resultAsString})");
+                CollectionAssert.AreEqual(
+                    null,
+                    result,
+                    $"For an empty input collection the result shall be null, but it was ({resultAsString})"
+                );
             }
             catch (InvalidOperationException ex)
             {
-                Assert.Fail($"For an empty input collection the result shall be null, but an exception was thrown: {ex.Message}");
+                Assert.Fail(
+                    $"For an empty input collection the result shall be null, but an exception was thrown: {ex.Message}"
+                );
             }
         }
     }
