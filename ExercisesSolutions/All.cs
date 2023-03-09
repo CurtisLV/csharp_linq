32265880bd0852e848920489f2762bda50d6b618
CurtisLV <curtislv@gmail.com>
2023-03-09 21:42:34 +0200

    09.03 12

--- ExercisesSolutions/All.cs
+++ ExercisesSolutions/All.cs
@@ -15,31 +15,24 @@ namespace ExercisesSolutions
         //Coding Exercise 2
         public static bool AreAllPetsOfTheSameType(IEnumerable<Pet> pets)
         {
-            return
-                pets.All(pet => pet.PetType == PetType.Dog) ||
-                pets.All(pet => pet.PetType == PetType.Cat) ||
-                pets.All(pet => pet.PetType == PetType.Fish);
+            return pets.All(pet => pet.PetType == PetType.Dog)
+                || pets.All(pet => pet.PetType == PetType.Cat)
+                || pets.All(pet => pet.PetType == PetType.Fish);
         }
 
         //Refactoring challenge
-        public static bool AreAllPetsOfTheSameType_AlternativeImplementation(
-            IEnumerable<Pet> pets)
+        public static bool AreAllPetsOfTheSameType_AlternativeImplementation(IEnumerable<Pet> pets)
         {
             var allPetTypes = Enum.GetValues(typeof(PetType)).Cast<PetType>();
             return allPetTypes.Any(petType => pets.All(pet => pet.PetType == petType));
         }
 
-        public static bool 
-            AreAllWordsOfTheSameLength_Refactored(
-                List<string> words)
+        public static bool AreAllWordsOfTheSameLength_Refactored(List<string> words)
         {
-            return !words.Any() || 
-                words.All(word => 
-                    word.Length == words[0].Length);
+            return !words.Any() || words.All(word => word.Length == words[0].Length);
         }
 
-        public static bool AreAllWordsOfTheSameLength(
-            List<string> words)
+        public static bool AreAllWordsOfTheSameLength(List<string> words)
         {
             if (words.Count == 0 || words.Count == 1)
             {
