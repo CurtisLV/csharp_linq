052ac3c781ed0927168c85dd98bf3e98ee5f5edb
CurtisLV <curtislv@gmail.com>
2023-03-10 19:39:16 +0200

    10.03. 9

--- ExercisesTests/CollectionTypeChange_Exercise2_Tests.cs
+++ ExercisesTests/CollectionTypeChange_Exercise2_Tests.cs
@@ -16,12 +16,18 @@ namespace ExercisesTests
             var expectedResult1 = new int[] { 2, 4, 6 };
             var expectedResult2 = new int[] { 1, 5, 7, 9 };
 
-            CollectionAssert.AreEqual(expectedResult1, result[true], 
-                $"result[true] shall be {EnumerableToString(expectedResult1)} but it was" +
-                $" {EnumerableToString(result[true])}");
-            CollectionAssert.AreEqual(expectedResult2, result[false], 
-                $"result[false] shall be {EnumerableToString(expectedResult2)} but it was" +
-                $" {EnumerableToString(result[false])}");
+            CollectionAssert.AreEqual(
+                expectedResult1,
+                result[true],
+                $"result[true] shall be {EnumerableToString(expectedResult1)} but it was"
+                    + $" {EnumerableToString(result[true])}"
+            );
+            CollectionAssert.AreEqual(
+                expectedResult2,
+                result[false],
+                $"result[false] shall be {EnumerableToString(expectedResult2)} but it was"
+                    + $" {EnumerableToString(result[false])}"
+            );
         }
     }
 }
