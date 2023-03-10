4db544dc71b67accf5542b669bfa487f336ae498
CurtisLV <curtislv@gmail.com>
2023-03-10 19:21:50 +0200

    10.03. 4

--- ExercisesSolutions/CollectionTypeChange.cs
+++ ExercisesSolutions/CollectionTypeChange.cs
@@ -8,51 +8,53 @@ namespace ExercisesSolutions
     {
         //Coding Exercise 1
         public static Dictionary<string, int?> ParseToNumbersAndStoreInDictionary(
-            IEnumerable<string> words)
+            IEnumerable<string> words
+        )
         {
-            return words.Distinct().ToDictionary(
-                 word => word,
-                 word => {
-                     int parsingResult;
+            return words
+                .Distinct()
+                .ToDictionary(
+                    word => word,
+                    word =>
+                    {
+                        int parsingResult;
 
-                     return int.TryParse(word, out parsingResult) ?
-                     parsingResult :
-                     (int?)null;
-                 });
+                        return int.TryParse(word, out parsingResult) ? parsingResult : (int?)null;
+                    }
+                );
         }
 
         //Coding Exercise 2
-        public static ILookup<bool, int> CreateLookupByDivisibilityBy2(
-            IEnumerable<int> input)
+        public static ILookup<bool, int> CreateLookupByDivisibilityBy2(IEnumerable<int> input)
         {
-            return input
-                .ToLookup(
-                number => number % 2 == 0,
-                number => number);
+            return input.ToLookup(number => number % 2 == 0, number => number);
         }
 
         //Refactoring challenge
-        public static Dictionary<string, double>
-             GetStudentsAverageMarks_Refactored(
-                 IEnumerable<Student> students)
+        public static Dictionary<string, double> GetStudentsAverageMarks_Refactored(
+            IEnumerable<Student> students
+        )
         {
             return students.ToDictionary(
-                student => $"{student.FirstName} {student.LastName} born on" +
-                    $" {student.DateOfBirth.ToString("d")}",
-                student => student.Marks.Any() ? student.Marks.Average() : 0);
+                student =>
+                    $"{student.FirstName} {student.LastName} born on"
+                    + $" {student.DateOfBirth.ToString("d")}",
+                student => student.Marks.Any() ? student.Marks.Average() : 0
+            );
         }
 
         //do not modify this method
-        public static Dictionary<string, double>
-            GetStudentsAverageMarks(
-                IEnumerable<Student> students)
+        public static Dictionary<string, double> GetStudentsAverageMarks(
+            IEnumerable<Student> students
+        )
         {
             var result = new Dictionary<string, double>();
             foreach (var student in students)
             {
-                var studentData = $"{student.FirstName} " +
-                    $"{student.LastName} born on" +
-                    $" {student.DateOfBirth.ToString("d")}";
+                var studentData =
+                    $"{student.FirstName} "
+                    + $"{student.LastName} born on"
+                    + $" {student.DateOfBirth.ToString("d")}";
 
                 var marksSum = 0;
                 foreach (var mark in student.Marks)
@@ -60,9 +62,7 @@ namespace ExercisesSolutions
                     marksSum += mark;
                 }
                 var marksCount = student.Marks.Count();
-                var averageMark = marksCount == 0 ?
-                    0 :
-                    marksSum / (float)marksCount;
+                var averageMark = marksCount == 0 ? 0 : marksSum / (float)marksCount;
                 result[studentData] = averageMark;
             }
             return result;
