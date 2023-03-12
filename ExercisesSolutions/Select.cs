0621605498bb8eeae93ac973d837b63329962402
CurtisLV <curtislv@gmail.com>
2023-03-12 10:56:22 +0200

    12.03 1

--- ExercisesSolutions/Select.cs
+++ ExercisesSolutions/Select.cs
@@ -9,61 +9,70 @@ namespace ExercisesSolutions
         //Coding Exercise 1
         public static IEnumerable<int> GetNumbers(IEnumerable<object> objects)
         {
-            return objects.OfType<int>().Concat(
-                objects.OfType<string>()
-                .Select(text => {
-                    int result;
-                    return int.TryParse(text, out result) ? result : (int?)null;
-                })
-                .Where(nullableNumber => nullableNumber != null)
-                .Select(nullableNumber => nullableNumber.Value))
+            return objects
+                .OfType<int>()
+                .Concat(
+                    objects
+                        .OfType<string>()
+                        .Select(text =>
+                        {
+                            int result;
+                            return int.TryParse(text, out result) ? result : (int?)null;
+                        })
+                        .Where(nullableNumber => nullableNumber != null)
+                        .Select(nullableNumber => nullableNumber.Value)
+                )
                 .OrderBy(number => number);
         }
 
         //Coding Exercise 2
         public static IEnumerable<Person> PeopleFromString(string input)
         {
-            return input.Split(';')
-               .Select(personData =>
-               {
-                   try
-                   {
-                       var split = personData.Split(',');
-                       var fullName = split[0].Split(' ');
-                       var firstName = fullName[0];
-                       var lastName = fullName[1];
-                       var dateOfBirth = DateTime.Parse(split[1]);
+            return input
+                .Split(';')
+                .Select(personData =>
+                {
+                    try
+                    {
+                        var split = personData.Split(',');
+                        var fullName = split[0].Split(' ');
+                        var firstName = fullName[0];
+                        var lastName = fullName[1];
+                        var dateOfBirth = DateTime.Parse(split[1]);
 
-                       return new Person
-                       {
-                           FirstName = firstName,
-                           LastName = lastName,
-                           DateOfBirth = dateOfBirth
-                       };
-                   }
-                   catch (Exception)
-                   {
-                       return null;
-                   }
-               })
-               .Where(person => person != null);
+                        return new Person
+                        {
+                            FirstName = firstName,
+                            LastName = lastName,
+                            DateOfBirth = dateOfBirth
+                        };
+                    }
+                    catch (Exception)
+                    {
+                        return null;
+                    }
+                })
+                .Where(person => person != null);
         }
 
         //Refactoring challenge
         public static TimeSpan TotalDurationOfSongs_Refactored(string allSongsDuration)
         {
-            return string.IsNullOrEmpty(allSongsDuration) ?
-                new TimeSpan() :
-                TimeSpan.FromSeconds(
-                allSongsDuration.Split(',')
-                .Select(songDurationAsString => TimeSpan.ParseExact(
-                    songDurationAsString, @"m\:ss", null))
-                .Sum(timeSpan => timeSpan.TotalSeconds));
+            return string.IsNullOrEmpty(allSongsDuration)
+                ? new TimeSpan()
+                : TimeSpan.FromSeconds(
+                    allSongsDuration
+                        .Split(',')
+                        .Select(
+                            songDurationAsString =>
+                                TimeSpan.ParseExact(songDurationAsString, @"m\:ss", null)
+                        )
+                        .Sum(timeSpan => timeSpan.TotalSeconds)
+                );
         }
 
         //do not modify this method
-        public static TimeSpan TotalDurationOfSongs(
-            string allSongsDuration)
+        public static TimeSpan TotalDurationOfSongs(string allSongsDuration)
         {
             if (string.IsNullOrEmpty(allSongsDuration))
             {
@@ -74,8 +83,7 @@ namespace ExercisesSolutions
             var totalDuration = 0d;
             foreach (var durationString in durationStrings)
             {
-                var timeSpan = TimeSpan.ParseExact(
-                    durationString, @"m\:ss", null);
+                var timeSpan = TimeSpan.ParseExact(durationString, @"m\:ss", null);
                 totalDuration += timeSpan.TotalSeconds;
             }
 
@@ -95,9 +103,9 @@ namespace ExercisesSolutions
 
             public bool Equals(Person other)
             {
-                return FirstName == other.FirstName &&
-                       LastName == other.LastName &&
-                       DateOfBirth == other.DateOfBirth;
+                return FirstName == other.FirstName
+                    && LastName == other.LastName
+                    && DateOfBirth == other.DateOfBirth;
             }
         }
     }
