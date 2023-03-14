a7eb04ffff1eb66430353598be176a53b1343a6a
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-03-14 17:27:57 +0200

    14.03. 9

--- Exercises/SelectMany.cs
+++ Exercises/SelectMany.cs
@@ -50,7 +50,17 @@ namespace Exercises
          */
         public static IEnumerable<string> BestMarksAndStudents(IEnumerable<Student> students)
         {
-            //
+            return students
+                .SelectMany(
+                    student => student.Marks,
+                    (student, mark) => new { Student = student, Mark = mark }
+                )
+                .OrderByDescending(studentMarkPair => studentMarkPair.Mark)
+                .ThenBy(studentMarkPair => studentMarkPair.Student.Name)
+                .Take(5)
+                .Select(
+                    studentMarkPair => $"{studentMarkPair.Student.Name}: {studentMarkPair.Mark}"
+                );
         }
 
         //Refactoring challenge
