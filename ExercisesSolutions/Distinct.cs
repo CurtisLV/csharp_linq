8845c2c1bf55ea093ebf54cfd98b26817587c885
CurtisLV <curtislv@gmail.com>
2023-03-15 23:20:40 +0200

    15.03 3

--- ExercisesSolutions/Distinct.cs
+++ ExercisesSolutions/Distinct.cs
@@ -13,24 +13,25 @@ namespace ExercisesSolutions
 
         //Coding Exercise 2
         public static IEnumerable<T> GetCollectionWithMostDuplicates<T>(
-            IEnumerable<IEnumerable<T>> collections)
+            IEnumerable<IEnumerable<T>> collections
+        )
         {
             return collections
-           .OrderBy(collection => collection.Count() - collection.Distinct().Count())
-           .ThenByDescending(collection => collection.Count())
-           .LastOrDefault();
+                .OrderBy(collection => collection.Count() - collection.Distinct().Count())
+                .ThenByDescending(collection => collection.Count())
+                .LastOrDefault();
         }
 
         //Refactoring challenge
         public static IEnumerable<string> GetWordsShorterThan5Letters_Refactored(
-            IEnumerable<string> words)
+            IEnumerable<string> words
+        )
         {
             return words.Distinct().Where(word => word.Length < 5);
         }
 
         //do not modify this method
-        public static IEnumerable<string> GetWordsShorterThan5Letters(
-            IEnumerable<string> words)
+        public static IEnumerable<string> GetWordsShorterThan5Letters(IEnumerable<string> words)
         {
             var result = new List<string>();
             foreach (var word in words)
