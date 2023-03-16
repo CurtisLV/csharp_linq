36da104997ddbcf797c313d917d03b1421d98bda
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-03-16 18:57:25 +0200

    16.03. 4

--- ExercisesTests/GeneratingNewCollection_RefactoringChallenge_Tests.cs
+++ ExercisesTests/GeneratingNewCollection_RefactoringChallenge_Tests.cs
@@ -13,7 +13,11 @@ namespace ExercisesTests
             var result = GeneratingNewCollection.DoubleLetters(3);
             var expectedResult = new[] { "AA", "AB", "AC", "BA", "BB", "BC", "CA", "CB", "CC" };
 
-            CollectionAssert.AreEqual(expectedResult, result, $"For 3 first letters, the result shall be {EnumerableToString(expectedResult)} but it was {EnumerableToString(result)}");
+            CollectionAssert.AreEqual(
+                expectedResult,
+                result,
+                $"For 3 first letters, the result shall be {EnumerableToString(expectedResult)} but it was {EnumerableToString(result)}"
+            );
         }
 
         [Test]
@@ -22,7 +26,11 @@ namespace ExercisesTests
             var result = GeneratingNewCollection.DoubleLetters_Refactored(3);
             var expectedResult = new[] { "AA", "AB", "AC", "BA", "BB", "BC", "CA", "CB", "CC" };
 
-            CollectionAssert.AreEqual(expectedResult, result, $"For 3 first letters, the result shall be {EnumerableToString(expectedResult)} but it was {EnumerableToString(result)}");
+            CollectionAssert.AreEqual(
+                expectedResult,
+                result,
+                $"For 3 first letters, the result shall be {EnumerableToString(expectedResult)} but it was {EnumerableToString(result)}"
+            );
         }
 
         [Test]
@@ -31,7 +39,11 @@ namespace ExercisesTests
             var result = GeneratingNewCollection.DoubleLetters(0);
             var expectedResult = new string[0];
 
-            CollectionAssert.AreEqual(expectedResult, result, $"For 0 first letters, the result shall be empty, but it was {EnumerableToString(result)}");
+            CollectionAssert.AreEqual(
+                expectedResult,
+                result,
+                $"For 0 first letters, the result shall be empty, but it was {EnumerableToString(result)}"
+            );
         }
 
         [Test]
@@ -40,7 +52,11 @@ namespace ExercisesTests
             var result = GeneratingNewCollection.DoubleLetters_Refactored(0);
             var expectedResult = new string[0];
 
-            CollectionAssert.AreEqual(expectedResult, result, $"For 0 first letters, the result shall be empty, but it was {EnumerableToString(result)}");
+            CollectionAssert.AreEqual(
+                expectedResult,
+                result,
+                $"For 0 first letters, the result shall be empty, but it was {EnumerableToString(result)}"
+            );
         }
     }
 }
