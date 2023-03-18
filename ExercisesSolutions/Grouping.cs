e3ade8cc43d6a228d5d01fa3f3ff12c205b94be6
CurtisLV <curtislv@gmail.com>
2023-03-18 15:58:41 +0200

    18.03 6

--- ExercisesSolutions/Grouping.cs
+++ ExercisesSolutions/Grouping.cs
@@ -9,34 +9,35 @@ namespace ExercisesSolutions
         //Coding Exercise 1
         public static char? GetTheMostFrequentCharacter(string text)
         {
-            return string.IsNullOrEmpty(text) ?
-                (char?)null :
-                text
-                .ToLower()
-                .GroupBy(character => character)
-                .OrderByDescending(characters => characters.Count())
-                .First()
-                .Key;
+            return string.IsNullOrEmpty(text)
+                ? (char?)null
+                : text.ToLower()
+                    .GroupBy(character => character)
+                    .OrderByDescending(characters => characters.Count())
+                    .First()
+                    .Key;
         }
 
         //Coding Exercise 2
         public static PetType? FindTheHeaviestPetType(IEnumerable<Pet> pets)
         {
-            return pets.Any() ?
-                pets.GroupBy(pet => pet.PetType)
-                .OrderBy(group => group.Average(pet => pet.Weight))
-                .Last().Key :
-                (PetType?)null;
+            return pets.Any()
+                ? pets.GroupBy(pet => pet.PetType)
+                    .OrderBy(group => group.Average(pet => pet.Weight))
+                    .Last()
+                    .Key
+                : (PetType?)null;
         }
 
         //Refactoring challenge
         public static IEnumerable<string> CountPets_Refactored(string petsData)
         {
-            return string.IsNullOrEmpty(petsData) ?
-                Enumerable.Empty<string>() :
-                petsData.Split(',')
-                .GroupBy(word => word)
-                .Select(group => $"{group.Key}:{group.Count()}");
+            return string.IsNullOrEmpty(petsData)
+                ? Enumerable.Empty<string>()
+                : petsData
+                    .Split(',')
+                    .GroupBy(word => word)
+                    .Select(group => $"{group.Key}:{group.Count()}");
         }
 
         //do not modify this method
