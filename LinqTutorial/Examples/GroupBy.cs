5bf903fdc32d3a745f3884069d670a7f837cf3d5
CurtisLV <curtislv@gmail.com>
2023-03-18 15:58:30 +0200

    18.03 5

--- LinqTutorial/Examples/GroupBy.cs
+++ LinqTutorial/Examples/GroupBy.cs
@@ -15,16 +15,14 @@ namespace LinqTutorial.MethodSyntax
             //with PetType as the Key
             //and sum of weights of Pets of this type
             //we will need to group all pets by PetType
-            var petsGroupedByType = Data.Pets
-                .GroupBy(
-                pet => pet.PetType,
-                pet => pet.Weight);
+            var petsGroupedByType = Data.Pets.GroupBy(pet => pet.PetType, pet => pet.Weight);
 
             //GroupBy creates a IEnumerable<IGrouping<PetType, float>>
             //now we need to sum up weights of each group
             var petTypesWeights = petsGroupedByType.ToDictionary(
                 grouping => grouping.Key,
-                grouping => grouping.Sum());
+                grouping => grouping.Sum()
+            );
             Printer.Print(petTypesWeights, nameof(petTypesWeights));
 
             //if we omitted the second param, the values in each group
@@ -36,9 +34,13 @@ namespace LinqTutorial.MethodSyntax
             var personInitialsPetsMapping = Data.People
                 .GroupBy(person => person.Name[0])
                 .ToDictionary(
-                grouping => grouping.Key,
-                grouping => string.Join(", ", grouping.SelectMany(person => person.Pets)
-                    .Select(pet => pet.Name)));
+                    grouping => grouping.Key,
+                    grouping =>
+                        string.Join(
+                            ", ",
+                            grouping.SelectMany(person => person.Pets).Select(pet => pet.Name)
+                        )
+                );
 
             Printer.Print(personInitialsPetsMapping, nameof(personInitialsPetsMapping));
 
@@ -50,21 +52,28 @@ namespace LinqTutorial.MethodSyntax
             //with information about minimal and maximal weight in each group
             var weightGroups = Data.Pets
                 .GroupBy(
-                pet => Math.Floor(pet.Weight),
-                (floorOfWeight, allPetsInThisGroup) => new
-                {
-                    FloorOfWeight = floorOfWeight,
-                    MinWeightInThisGroup = allPetsInThisGroup.Min(pet => pet.Weight),
-                    MaxWeightInThisGroup = allPetsInThisGroup.Max(pet => pet.Weight)
-                })
+                    pet => Math.Floor(pet.Weight),
+                    (floorOfWeight, allPetsInThisGroup) =>
+                        new
+                        {
+                            FloorOfWeight = floorOfWeight,
+                            MinWeightInThisGroup = allPetsInThisGroup.Min(pet => pet.Weight),
+                            MaxWeightInThisGroup = allPetsInThisGroup.Max(pet => pet.Weight)
+                        }
+                )
                 .OrderBy(weightGroup => weightGroup.FloorOfWeight);
 
-            Console.WriteLine(string.Join(
-                "\n",
-                weightGroups.Select(weightGroup =>
-                $"Group: {weightGroup.FloorOfWeight}," +
-                $" Min: {weightGroup.MinWeightInThisGroup}," +
-                $" Max: {weightGroup.MaxWeightInThisGroup}")));
+            Console.WriteLine(
+                string.Join(
+                    "\n",
+                    weightGroups.Select(
+                        weightGroup =>
+                            $"Group: {weightGroup.FloorOfWeight},"
+                            + $" Min: {weightGroup.MinWeightInThisGroup},"
+                            + $" Max: {weightGroup.MaxWeightInThisGroup}"
+                    )
+                )
+            );
         }
 
         public static class QuerySyntax
@@ -77,28 +86,32 @@ namespace LinqTutorial.MethodSyntax
                 //and sum of weights of Pets of this type
                 //we will need to group all pets by PetType
 
-                var petsGroupedByType = from pet in Data.Pets
-                                        group pet by pet.PetType;
+                var petsGroupedByType = from pet in Data.Pets group pet by pet.PetType;
 
                 //GroupBy creates a IEnumerable<IGrouping<PetType, Pet>>
                 //now we need to sum up weights of each group
                 var petTypesWeights = petsGroupedByType.ToDictionary(
                     grouping => grouping.Key,
-                    grouping => grouping.Sum(pet => pet.Weight));
+                    grouping => grouping.Sum(pet => pet.Weight)
+                );
                 Printer.Print(petTypesWeights, nameof(petTypesWeights));
 
                 //now, let's group People by their names first letter
                 //and create a Dictionary where the first letter will be the key
                 //and those People Pets' Names will be the value
-                var personInitialsPetsMapping =
-                    (from person in Data.People
-                     group person by person.Name.First())
-                     .ToDictionary(
-                        grouping => grouping.Key,
-                        grouping => string.Join(",",
+                var personInitialsPetsMapping = (
+                    from person in Data.People
+                    group person by person.Name.First()
+                ).ToDictionary(
+                    grouping => grouping.Key,
+                    grouping =>
+                        string.Join(
+                            ",",
                             from person in grouping
                             from pet in person.Pets
-                            select pet.Name));
+                            select pet.Name
+                        )
+                );
 
                 Printer.Print(personInitialsPetsMapping, nameof(personInitialsPetsMapping));
 
@@ -106,27 +119,27 @@ namespace LinqTutorial.MethodSyntax
                 //now let's group Pets by the floor of their Weight
                 //and then let's create print all those groups
                 //with information about minimal and maximal weight in each group
-                var petWeightGroups = from pet in Data.Pets
-                                      group pet by Math.Floor(pet.Weight) into grouping
-                                      orderby grouping.Key
-                                      let petsOrderedByWeight = from pet in grouping
-                                                                orderby pet.Weight
-                                                                select pet
-                                      select new
-                                      {
-                                          Key = grouping.Key,
-                                          LightestPet = petsOrderedByWeight.First(),
-                                          HeaviestPet = petsOrderedByWeight.Last(),
-                                      };
+                var petWeightGroups =
+                    from pet in Data.Pets
+                    group pet by Math.Floor(pet.Weight) into grouping
+                    orderby grouping.Key
+                    let petsOrderedByWeight = from pet in grouping
+                    orderby pet.Weight
+                    select pet
+                    select new
+                    {
+                        Key = grouping.Key,
+                        LightestPet = petsOrderedByWeight.First(),
+                        HeaviestPet = petsOrderedByWeight.Last(),
+                    };
 
-                var petsWeightsGroupsAsStrings = from petWeightGroup in petWeightGroups
-                                                 select $"Weight category: {petWeightGroup.Key}, " +
-                                                 $"heaviestPet: {petWeightGroup.HeaviestPet.Name}, " +
-                                                 $"lightestPet: {petWeightGroup.LightestPet.Name}";
+                var petsWeightsGroupsAsStrings =
+                    from petWeightGroup in petWeightGroups
+                    select $"Weight category: {petWeightGroup.Key}, "
+                        + $"heaviestPet: {petWeightGroup.HeaviestPet.Name}, "
+                        + $"lightestPet: {petWeightGroup.LightestPet.Name}";
                 Printer.Print(petsWeightsGroupsAsStrings, nameof(petsWeightsGroupsAsStrings));
-
             }
         }
     }
 }
-
