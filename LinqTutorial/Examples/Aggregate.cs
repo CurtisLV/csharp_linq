9c8109adbc84de1f9507e8c291dc1c7a1a4bf870
CurtisLV <curtislv@gmail.com>
2023-03-21 22:48:35 +0200

    21.03 6

--- LinqTutorial/Examples/Aggregate.cs
+++ LinqTutorial/Examples/Aggregate.cs
@@ -4,7 +4,6 @@ using Utilities;
 
 namespace LinqTutorial.MethodSyntax
 {
-
     static class Aggregate
     {
         //System.Linq.Enumerable.Aggregate
@@ -22,9 +21,12 @@ namespace LinqTutorial.MethodSyntax
 
             //let's find the longest word in the collection
             var sentence = "The quick brown fox jumps over the lazy dog";
-            var longestWord = sentence.Split(" ")
-                .Aggregate((longestSoFar, nextWord) => 
-                nextWord.Length > longestSoFar.Length ? nextWord : longestSoFar);
+            var longestWord = sentence
+                .Split(" ")
+                .Aggregate(
+                    (longestSoFar, nextWord) =>
+                        nextWord.Length > longestSoFar.Length ? nextWord : longestSoFar
+                );
             Console.WriteLine($"longest word is '{longestWord}'");
 
             //let's use Aggregate to merge letters with a separator
@@ -35,26 +37,29 @@ namespace LinqTutorial.MethodSyntax
 
             //we can use non-default initial value (so-called "seed")
             //we can also implement Count using Aggregate
-            var countOfWords = sentence.Split(" ")
-                .Aggregate(
-                0,
-                (totalCount, nextWord) => totalCount + 1);
+            var countOfWords = sentence
+                .Split(" ")
+                .Aggregate(0, (totalCount, nextWord) => totalCount + 1);
             Console.WriteLine($"countOfWords: {countOfWords}");
 
             //let's create a list of all lenghts of words from the sentence
-            var lenghtsOfWords = sentence.Split(" ")
+            var lenghtsOfWords = sentence
+                .Split(" ")
                 .Aggregate(
-                Enumerable.Empty<int>(),
-                (wordsLengthsLists, nextWord) => wordsLengthsLists.Append(nextWord.Length));
+                    Enumerable.Empty<int>(),
+                    (wordsLengthsLists, nextWord) => wordsLengthsLists.Append(nextWord.Length)
+                );
             Printer.Print(lenghtsOfWords, nameof(lenghtsOfWords));
 
             //let's calculate the factorial of 10
             //10! = 10*9*8*7*6*5*4*3*2*1
             var factorialBase = 10;
-            var factorial = Enumerable.Range(1, factorialBase - 1)
+            var factorial = Enumerable
+                .Range(1, factorialBase - 1)
                 .Aggregate(
                     10,
-                    (factorialSoFar, nextNumber) => factorialSoFar * (factorialBase - nextNumber));
+                    (factorialSoFar, nextNumber) => factorialSoFar * (factorialBase - nextNumber)
+                );
             Console.WriteLine($"factorial: {factorial}");
         }
     }
