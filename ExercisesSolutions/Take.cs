b4000f3f5d235178e3605ef5f9919bb993fe3975
CurtisLV <curtislv@gmail.com>
2023-03-21 22:48:18 +0200

    21.03 4

--- ExercisesSolutions/Take.cs
+++ ExercisesSolutions/Take.cs
@@ -9,30 +9,32 @@ namespace ExercisesSolutions
         //Coding Exercise 1
         public static IEnumerable<int> TakeSome(IEnumerable<int> numbers)
         {
-            return numbers.Count() < 10 ?
-                numbers.Take(3) :
-                numbers.Count() < 100 ? numbers.Take(30) : numbers;
+            return numbers.Count() < 10
+                ? numbers.Take(3)
+                : numbers.Count() < 100
+                    ? numbers.Take(30)
+                    : numbers;
         }
 
         //Coding Exercise 2
         public static IEnumerable<Pet> GetGivenPercentOfHeaviestPets(
-            IEnumerable<Pet> pets, int percent)
+            IEnumerable<Pet> pets,
+            int percent
+        )
         {
-            return pets
-                .OrderByDescending(pet => pet.Weight)
-                .Take(pets.Count() * percent / 100);
+            return pets.OrderByDescending(pet => pet.Weight).Take(pets.Count() * percent / 100);
         }
 
         //Refactoring challenge
         public static IEnumerable<DateTime> GetDatesBeforeXXCentury_Refactored(
-            IEnumerable<DateTime> dates)
+            IEnumerable<DateTime> dates
+        )
         {
             return dates.TakeWhile(date => date.Year < 1901);
         }
 
         //do not modify this method
-        public static IEnumerable<DateTime> GetDatesBeforeXXCentury(
-            IEnumerable<DateTime> dates)
+        public static IEnumerable<DateTime> GetDatesBeforeXXCentury(IEnumerable<DateTime> dates)
         {
             var result = new List<DateTime>();
             foreach (var date in dates)
