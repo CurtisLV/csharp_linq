fdae32db86d27e83cd9c6a33a9c9cb69eb7dab92
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-03-22 16:11:58 +0200

    22.03 8

--- ExercisesTests/Join_RefactoringChallenge_Tests.cs
+++ ExercisesTests/Join_RefactoringChallenge_Tests.cs
@@ -15,16 +15,16 @@ namespace ExercisesTests
         {
             var people = new Person[]
             {
-              new Person(1, "John Smith"),
-              new Person(2, "Stephanie Green"),
-              new Person(3, "Martin Brown")
+                new Person(1, "John Smith"),
+                new Person(2, "Stephanie Green"),
+                new Person(3, "Martin Brown")
             };
 
             var houses = new House[]
             {
-              new House(2, "White Cottage, 18 Miners Overlook"),
-              new House(3, "Hilltop Mansion, 234 Maple Road"),
-              new House(3, "Beach Farm, 10 Seaside Street"),
+                new House(2, "White Cottage, 18 Miners Overlook"),
+                new House(3, "Hilltop Mansion, 234 Maple Road"),
+                new House(3, "Beach Farm, 10 Seaside Street"),
             };
             var result = Join.GetHousesData(people, houses);
             var expectedResult = new Dictionary<House, Person>
@@ -34,7 +34,11 @@ namespace ExercisesTests
                 [houses[2]] = people[2],
             };
 
-            CollectionAssert.AreEquivalent(expectedResult, result, $"For PEOPLE {EnumerableToString(people)}, and HOUSES  {EnumerableToString(houses)}  the RESULT shall be {DictionaryToString(expectedResult)}, BUT IT WAS {DictionaryToString(result)}");
+            CollectionAssert.AreEquivalent(
+                expectedResult,
+                result,
+                $"For PEOPLE {EnumerableToString(people)}, and HOUSES  {EnumerableToString(houses)}  the RESULT shall be {DictionaryToString(expectedResult)}, BUT IT WAS {DictionaryToString(result)}"
+            );
         }
 
         [Test]
@@ -42,16 +46,16 @@ namespace ExercisesTests
         {
             var people = new Person[]
             {
-              new Person(1, "John Smith"),
-              new Person(2, "Stephanie Green"),
-              new Person(3, "Martin Brown")
+                new Person(1, "John Smith"),
+                new Person(2, "Stephanie Green"),
+                new Person(3, "Martin Brown")
             };
 
             var houses = new House[]
             {
-              new House(2, "White Cottage"),
-              new House(3, "Hilltop Mansion"),
-              new House(3, "Beach Farm"),
+                new House(2, "White Cottage"),
+                new House(3, "Hilltop Mansion"),
+                new House(3, "Beach Farm"),
             };
             var result = Join.GetHousesData_Refactored(people, houses);
             var expectedResult = new Dictionary<House, Person>
@@ -61,7 +65,11 @@ namespace ExercisesTests
                 [houses[2]] = people[2],
             };
 
-            CollectionAssert.AreEquivalent(expectedResult, result, $"For PEOPLE {EnumerableToString(people)}, and HOUSES  {EnumerableToString(houses)}  the RESULT shall be {DictionaryToString(expectedResult)}, BUT IT WAS {DictionaryToString(result)}");
+            CollectionAssert.AreEquivalent(
+                expectedResult,
+                result,
+                $"For PEOPLE {EnumerableToString(people)}, and HOUSES  {EnumerableToString(houses)}  the RESULT shall be {DictionaryToString(expectedResult)}, BUT IT WAS {DictionaryToString(result)}"
+            );
         }
     }
 }
