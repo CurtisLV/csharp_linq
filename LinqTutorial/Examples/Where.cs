7023493e9cdada76ee29b24e4bfefd445a2eb328
CurtisLV <curtislv@gmail.com>
2023-03-23 19:14:00 +0200

    23.03. 7

--- LinqTutorial/Examples/Where.cs
+++ LinqTutorial/Examples/Where.cs
@@ -16,7 +16,7 @@ namespace LinqTutorial
             var evenNumbers = numbers.Where(n => n % 2 == 0);
             Printer.Print(evenNumbers, nameof(evenNumbers));
 
-            //remember that we can use method's name only 
+            //remember that we can use method's name only
             //if it is not ambiguous which method to call
             var evenNumbers2 = numbers.Where(IsEven);
 
@@ -28,29 +28,36 @@ namespace LinqTutorial
             Printer.Print(allPetsHeavierThan100Kilos, nameof(allPetsHeavierThan100Kilos));
 
             //we can, of course, use more complex conditions:
-            var verySpecificPets = Data.Pets.Where(pet =>
-                (pet.PetType == PetType.Dog || pet.PetType == PetType.Cat) &&
-                pet.Name.Length > 4 &&
-                pet.Weight > 10 &&
-                pet.Id % 2 == 0);
+            var verySpecificPets = Data.Pets.Where(
+                pet =>
+                    (pet.PetType == PetType.Dog || pet.PetType == PetType.Cat)
+                    && pet.Name.Length > 4
+                    && pet.Weight > 10
+                    && pet.Id % 2 == 0
+            );
             Printer.Print(verySpecificPets, nameof(verySpecificPets));
 
             //we can access the index in the Where method
             //let's say the user selected some Pets in the UI:
             var indexesSelectedByUser = new[] { 0, 2, 4 };
-            var petsSelectedByUserLighterThan5Kilos = Data.Pets.Where((pet, index) =>
-                indexesSelectedByUser.Contains(index) && pet.Weight < 5);
-            Printer.Print(petsSelectedByUserLighterThan5Kilos,
-                nameof(petsSelectedByUserLighterThan5Kilos));
+            var petsSelectedByUserLighterThan5Kilos = Data.Pets.Where(
+                (pet, index) => indexesSelectedByUser.Contains(index) && pet.Weight < 5
+            );
+            Printer.Print(
+                petsSelectedByUserLighterThan5Kilos,
+                nameof(petsSelectedByUserLighterThan5Kilos)
+            );
 
-            // in LINQ we can often achieve one result in two ways. 
+            // in LINQ we can often achieve one result in two ways.
             // for example, to count all dogs heavier than 30 kilos we can do:
             // A
             var countOfDogsHeavierThan30KilosA = Data.Pets.Count(
-                pet => pet.PetType == PetType.Dog && pet.Weight > 30);
+                pet => pet.PetType == PetType.Dog && pet.Weight > 30
+            );
             //B
-            var countOfDogsHeavierThan30KilosB = Data.Pets.Where(
-                pet => pet.PetType == PetType.Dog && pet.Weight > 30).Count();
+            var countOfDogsHeavierThan30KilosB = Data.Pets
+                .Where(pet => pet.PetType == PetType.Dog && pet.Weight > 30)
+                .Count();
         }
 
         static bool IsEven(int number) => number % 2 == 0;
@@ -62,23 +69,26 @@ namespace LinqTutorial
             {
                 var numbers = new[] { 10, 1, 4, 17, 122 };
 
-                var evenNumbers = from number in numbers
-                                  where number % 2 == 0
-                                  select number;
+                var evenNumbers = from number in numbers where number % 2 == 0 select number;
                 Printer.Print(evenNumbers, nameof(evenNumbers));
 
-                var allPetsHeavierThan10Kilos = from pet in Data.Pets
-                                                where pet.Weight > 10
-                                                select pet;
+                var allPetsHeavierThan10Kilos =
+                    from pet in Data.Pets
+                    where pet.Weight > 10
+                    select pet;
                 Printer.Print(allPetsHeavierThan10Kilos, nameof(allPetsHeavierThan10Kilos));
 
                 //we can, of course, use more complex conditions:
-                var verySpecificPets = from pet in Data.Pets
-                                       where ((pet.PetType == PetType.Dog || pet.PetType == PetType.Cat) &&
-                                               pet.Name.Length > 4 &&
-                                               pet.Weight > 10 &&
-                                               pet.Id % 2 == 0)
-                                       select pet;
+                var verySpecificPets =
+                    from pet in Data.Pets
+                    where
+                        (
+                            (pet.PetType == PetType.Dog || pet.PetType == PetType.Cat)
+                            && pet.Name.Length > 4
+                            && pet.Weight > 10
+                            && pet.Id % 2 == 0
+                        )
+                    select pet;
 
                 Printer.Print(verySpecificPets, nameof(verySpecificPets));
 
@@ -87,20 +97,30 @@ namespace LinqTutorial
                 //we would have to use Method Syntax Select method as helper
                 var indexesSelectedByUser = new[] { 0, 2, 4 };
                 var petsSelectedByUserLighterThan5Kilos =
-                    from petIndexPair in Data.Pets.Select((pet, index) => new { Pet = pet, Index = index })
-                    where indexesSelectedByUser.Contains(petIndexPair.Index) && petIndexPair.Pet.Weight < 5
+                    from petIndexPair in Data.Pets.Select(
+                        (pet, index) => new { Pet = pet, Index = index }
+                    )
+                    where
+                        indexesSelectedByUser.Contains(petIndexPair.Index)
+                        && petIndexPair.Pet.Weight < 5
                     select petIndexPair.Pet;
 
-                Printer.Print(petsSelectedByUserLighterThan5Kilos,
-                    nameof(petsSelectedByUserLighterThan5Kilos));
+                Printer.Print(
+                    petsSelectedByUserLighterThan5Kilos,
+                    nameof(petsSelectedByUserLighterThan5Kilos)
+                );
 
                 // let's count all dogs heavier than 30 kilos:
-                var countOfAllDogsHeavierThan30KilosA =
-                    (from pet in Data.Pets
-                     where pet.PetType == PetType.Dog && pet.Weight > 30 select pet).Count();
+                var countOfAllDogsHeavierThan30KilosA = (
+                    from pet in Data.Pets
+                    where pet.PetType == PetType.Dog && pet.Weight > 30
+                    select pet
+                ).Count();
 
-                Printer.Print(countOfAllDogsHeavierThan30KilosA,
-                  nameof(countOfAllDogsHeavierThan30KilosA));
+                Printer.Print(
+                    countOfAllDogsHeavierThan30KilosA,
+                    nameof(countOfAllDogsHeavierThan30KilosA)
+                );
             }
         }
     }
