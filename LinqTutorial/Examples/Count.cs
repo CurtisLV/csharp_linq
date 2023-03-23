3806d7a44b8557384f54abcd83bd82f89ff321f2
CurtisLV <curtislv@gmail.com>
2023-03-23 19:11:45 +0200

    23.03. 1

--- LinqTutorial/Examples/Count.cs
+++ LinqTutorial/Examples/Count.cs
@@ -21,9 +21,9 @@ namespace LinqTutorial
             Console.WriteLine($"countOfPetsNamedBruce: {countOfPetsNamedBruce}");
 
             //we can, of course, define more complex predicate
-            var countOfAllSmallDogs = Data.Pets.Count(pet => 
-                pet.PetType == PetType.Dog && 
-                pet.Weight < 10);
+            var countOfAllSmallDogs = Data.Pets.Count(
+                pet => pet.PetType == PetType.Dog && pet.Weight < 10
+            );
             Console.WriteLine($"countOfAllSmallDogs: {countOfAllSmallDogs}");
 
             //count of all elements
