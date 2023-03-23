0ba2e43cdfd11b21b37f0c04d0a5e28ab20848de
CurtisLV <curtislv@gmail.com>
2023-03-23 19:12:59 +0200

    23.03. 3

--- LinqTutorial/Examples/GroupJoin.cs
+++ LinqTutorial/Examples/GroupJoin.cs
@@ -15,14 +15,14 @@ namespace LinqTutorial.MethodSyntax
             //PetName1 - appointmentDate1, appointmentDate2
             //PetName2 - appointmentDate3
             //PetName3 - appointmentDate4, appointmentDate5
-            var petsGroupedAppointments = Data.Pets
-                .GroupJoin(
-                    Data.VeterinaryClinicAppointments,
-                    pet => pet.Id,
-                    appointmet => appointmet.PetId,
-                    (pet, appointments) =>
-                    $"Pet: {pet.Name}, " +
-                    $"appointment dates: {string.Join(", ", appointments.Select(a => a.Date))}");
+            var petsGroupedAppointments = Data.Pets.GroupJoin(
+                Data.VeterinaryClinicAppointments,
+                pet => pet.Id,
+                appointmet => appointmet.PetId,
+                (pet, appointments) =>
+                    $"Pet: {pet.Name}, "
+                    + $"appointment dates: {string.Join(", ", appointments.Select(a => a.Date))}"
+            );
 
             Printer.Print(petsGroupedAppointments, nameof(petsGroupedAppointments));
 
@@ -31,19 +31,22 @@ namespace LinqTutorial.MethodSyntax
             //and the matched records from the right table
             //in this case we want to print all Pet's data,
             //even if they don't have an appointment planned
-            var leftJoin = Data.Pets.GroupJoin(
-                Data.VeterinaryClinicAppointments,
-                pet => pet.Id,
-                appointmet => appointmet.PetId,
-                (pet, appointments) => new { Pet = pet, Appointments = appointments })
+            var leftJoin = Data.Pets
+                .GroupJoin(
+                    Data.VeterinaryClinicAppointments,
+                    pet => pet.Id,
+                    appointmet => appointmet.PetId,
+                    (pet, appointments) => new { Pet = pet, Appointments = appointments }
+                )
                 //there can be zero or more appointments in the "petAppointments" collection
-                .SelectMany(petAppointments => petAppointments.Appointments.DefaultIfEmpty(),
-                //without DefaultOrEmpty there would be no appointments for a given pet
-                //so it wouldn't be included in results from SelectMany method
-                  (petAppointment, appointment) =>
-                  $"Pet name: {petAppointment.Pet.Name}, appointment date: {appointment?.Date}");
+                .SelectMany(
+                    petAppointments => petAppointments.Appointments.DefaultIfEmpty(),
+                    //without DefaultOrEmpty there would be no appointments for a given pet
+                    //so it wouldn't be included in results from SelectMany method
+                    (petAppointment, appointment) =>
+                        $"Pet name: {petAppointment.Pet.Name}, appointment date: {appointment?.Date}"
+                );
             Printer.Print(leftJoin, nameof(leftJoin));
         }
     }
 }
-
