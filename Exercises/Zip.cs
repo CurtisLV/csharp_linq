3eb07627bd2c2218e179f88a7e2f161a0b508c6a
CurtisLV <curtislv@gmail.com>
2023-03-28 19:43:17 +0300

    28.03. 1

--- Exercises/Zip.cs
+++ Exercises/Zip.cs
@@ -8,13 +8,13 @@ namespace Exercises
     {
         //Coding Exercise 1
         /*
-         Imagine you work on a program which imports data stored in Excel spreadsheets. 
+         Imagine you work on a program which imports data stored in Excel spreadsheets.
         There is a need to import dates, but each part of a date - year, month, and day -
         is stored in a separate column. The data from each column is imported into your
         program as a collection of integers.
 
-        Implement the BuildDates method, which given collections of 
-        years, months and days will build a collection of DateTimes. 
+        Implement the BuildDates method, which given collections of
+        years, months and days will build a collection of DateTimes.
         The result shall be ordered from the oldest date to the newest.
         
         For example, for the following input:
@@ -29,13 +29,14 @@ namespace Exercises
         
         Please assume the input is valid, which means:
             *all collections have the same length
-            *all values are valid, which means valid dates can be built from them 
+            *all values are valid, which means valid dates can be built from them
                 (for example months range from 1 to 12)
          */
         public static IEnumerable<DateTime> BuildDates(
             IEnumerable<int> years,
             IEnumerable<int> months,
-            IEnumerable<int> days)
+            IEnumerable<int> days
+        )
         {
             //TODO your code goes here
             throw new NotImplementedException();
@@ -43,8 +44,8 @@ namespace Exercises
 
         //Coding Exercise 2
         /*
-        Implement the GetDaysDifferencesBetweenDates method, which given an ordered 
-        collection of dates will return a collection of strings with information on 
+        Implement the GetDaysDifferencesBetweenDates method, which given an ordered
+        collection of dates will return a collection of strings with information on
         how many days have passed between dates in the collection.
 
         For example, for the following dates:
@@ -52,15 +53,15 @@ namespace Exercises
             *new DateTime(1899, 4, 12)
             *new DateTime(1899, 12, 31)
         
-        ...the result shall be:        
+        ...the result shall be:
             *"It's been 101 days between 1899-01-01 and 1914-04-12"
             *"It's been 263 days between 1914-04-12 and 1929-12-31"
         
         To format dates to string, please use the date.ToString("yyyy-MM-dd") formatting.
          */
-        public static IEnumerable<string>
-            GetDaysDifferencesBetweenDates(
-                IEnumerable<DateTime> dates)
+        public static IEnumerable<string> GetDaysDifferencesBetweenDates(
+            IEnumerable<DateTime> dates
+        )
         {
             //TODO your code goes here
             throw new NotImplementedException();
