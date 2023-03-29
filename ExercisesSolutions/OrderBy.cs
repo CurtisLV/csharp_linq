3f9782d571870690119a28da657e6c7c619c9c37
CurtisLV <curtislv@gmail.com>
2023-03-29 22:49:14 +0300

    29.03 4

--- ExercisesSolutions/OrderBy.cs
+++ ExercisesSolutions/OrderBy.cs
@@ -7,24 +7,19 @@ namespace ExercisesSolutions
     public static class OrderBy
     {
         //Coding Exercise 1
-        public static IEnumerable<string> OrderFromLongestToShortest(
-            IEnumerable<string> words)
+        public static IEnumerable<string> OrderFromLongestToShortest(IEnumerable<string> words)
         {
             return words.OrderByDescending(word => word.Length);
         }
 
         //Coding Exercise 2
-        public static IEnumerable<int> FirstEvenThenOddDescending(
-            IEnumerable<int> numbers)
+        public static IEnumerable<int> FirstEvenThenOddDescending(IEnumerable<int> numbers)
         {
-            return numbers
-                .OrderBy(number => number % 2 != 0)
-                .ThenByDescending(number => number);
+            return numbers.OrderBy(number => number % 2 != 0).ThenByDescending(number => number);
         }
 
         //Refactoring challenge
-        public static IEnumerable<DateTime> OrderByMonth_Refactored(
-            List<DateTime> dates)
+        public static IEnumerable<DateTime> OrderByMonth_Refactored(List<DateTime> dates)
         {
             return dates.OrderBy(date => date.Month);
         }
@@ -32,10 +27,12 @@ namespace ExercisesSolutions
         //do not modify this method
         public static IEnumerable<DateTime> OrderByMonth(List<DateTime> dates)
         {
-            dates.Sort((left, right) =>
-            {
-                return left.Month.CompareTo(right.Month);
-            });
+            dates.Sort(
+                (left, right) =>
+                {
+                    return left.Month.CompareTo(right.Month);
+                }
+            );
             return dates;
         }
     }
