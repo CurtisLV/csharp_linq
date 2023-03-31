1c4434fe8a65cee066c4b01379151b6ac594819f
CurtisLV <curtislv@gmail.com>
2023-03-31 18:04:21 +0300

    31.03 3

--- ExercisesTests/OrderBy_QuerySyntax_RefactoringChallenge_Tests.cs
+++ ExercisesTests/OrderBy_QuerySyntax_RefactoringChallenge_Tests.cs
@@ -11,13 +11,15 @@ namespace ExercisesTests
         [Test]
         public void FirstTest()
         {
-            var numbers = new[] {
+            var numbers = new[]
+            {
                 new DateTime(2019, 10, 11),
                 new DateTime(2020, 10, 11),
                 new DateTime(2021, 10, 11),
             };
 
-            var expectedResult = new[] {
+            var expectedResult = new[]
+            {
                 new DateTime(2020, 10, 11),
                 new DateTime(2021, 10, 11),
                 new DateTime(2019, 10, 11),
@@ -25,22 +27,28 @@ namespace ExercisesTests
 
             var result = OrderByQuerySyntax.OrderDatesByDayOfWeek(numbers);
 
-            CollectionAssert.AreEqual(expectedResult, result, $"For input " +
-                $"{EnumerableToString(numbers)} the result shall be" +
-                $" {EnumerableToString(expectedResult)} but it was" +
-                $" '{EnumerableToString(result)}'");
+            CollectionAssert.AreEqual(
+                expectedResult,
+                result,
+                $"For input "
+                    + $"{EnumerableToString(numbers)} the result shall be"
+                    + $" {EnumerableToString(expectedResult)} but it was"
+                    + $" '{EnumerableToString(result)}'"
+            );
         }
 
         [Test]
         public void OrderDatesByDayOfWeekShallWorkCorrectly2()
         {
-            var numbers = new[] {
+            var numbers = new[]
+            {
                 new DateTime(2018, 10, 11),
                 new DateTime(2017, 10, 11),
                 new DateTime(2016, 10, 11),
             };
 
-            var expectedResult = new[] {
+            var expectedResult = new[]
+            {
                 new DateTime(2016, 10, 11),
                 new DateTime(2017, 10, 11),
                 new DateTime(2018, 10, 11),
@@ -48,22 +56,28 @@ namespace ExercisesTests
 
             var result = OrderByQuerySyntax.OrderDatesByDayOfWeek(numbers);
 
-            CollectionAssert.AreEqual(expectedResult, result, $"For input " +
-                $"{EnumerableToString(numbers)} the result shall be " +
-                $"{EnumerableToString(expectedResult)} but it was " +
-                $"'{EnumerableToString(result)}'");
+            CollectionAssert.AreEqual(
+                expectedResult,
+                result,
+                $"For input "
+                    + $"{EnumerableToString(numbers)} the result shall be "
+                    + $"{EnumerableToString(expectedResult)} but it was "
+                    + $"'{EnumerableToString(result)}'"
+            );
         }
 
         [Test]
         public void OrderDatesByDayOfWeekShallWorkCorrectly1_Refactored()
         {
-            var numbers = new[] {
+            var numbers = new[]
+            {
                 new DateTime(2019, 10, 11),
                 new DateTime(2020, 10, 11),
                 new DateTime(2021, 10, 11),
             };
 
-            var expectedResult = new[] {
+            var expectedResult = new[]
+            {
                 new DateTime(2020, 10, 11),
                 new DateTime(2021, 10, 11),
                 new DateTime(2019, 10, 11),
@@ -71,22 +85,28 @@ namespace ExercisesTests
 
             var result = OrderByQuerySyntax.OrderDatesByDayOfWeek_Refactored(numbers);
 
-            CollectionAssert.AreEqual(expectedResult, result, $"For input " +
-                $"{EnumerableToString(numbers)} the result shall be " +
-                $"{EnumerableToString(expectedResult)} but it was " +
-                $"'{EnumerableToString(result)}'");
+            CollectionAssert.AreEqual(
+                expectedResult,
+                result,
+                $"For input "
+                    + $"{EnumerableToString(numbers)} the result shall be "
+                    + $"{EnumerableToString(expectedResult)} but it was "
+                    + $"'{EnumerableToString(result)}'"
+            );
         }
 
         [Test]
         public void OrderDatesByDayOfWeekShallWorkCorrectly2_Refactored()
         {
-            var numbers = new[] {
+            var numbers = new[]
+            {
                 new DateTime(2018, 10, 11),
                 new DateTime(2017, 10, 11),
                 new DateTime(2016, 10, 11),
             };
 
-            var expectedResult = new[] {
+            var expectedResult = new[]
+            {
                 new DateTime(2016, 10, 11),
                 new DateTime(2017, 10, 11),
                 new DateTime(2018, 10, 11),
@@ -94,10 +114,13 @@ namespace ExercisesTests
 
             var result = OrderByQuerySyntax.OrderDatesByDayOfWeek_Refactored(numbers);
 
-            CollectionAssert.AreEqual(expectedResult, result, 
-                $"For input {EnumerableToString(numbers)} the result shall be " +
-                $"{EnumerableToString(expectedResult)} but it was " +
-                $"'{EnumerableToString(result)}'");
+            CollectionAssert.AreEqual(
+                expectedResult,
+                result,
+                $"For input {EnumerableToString(numbers)} the result shall be "
+                    + $"{EnumerableToString(expectedResult)} but it was "
+                    + $"'{EnumerableToString(result)}'"
+            );
         }
     }
 }
