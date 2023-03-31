9eebd8ca7e4c03c1825cc1acb123bdfcc9b5b4da
CurtisLV <curtislv@gmail.com>
2023-03-31 18:02:52 +0300

    31.03 1

--- ExercisesSolutions/OrderByQuerySyntax.cs
+++ ExercisesSolutions/OrderByQuerySyntax.cs
@@ -7,44 +7,37 @@ namespace ExercisesSolutions
     public static class OrderByQuerySyntax
     {
         //Coding Exercise 1
-        public static IEnumerable<TimeSpan>
-            OrderFromLongestToShortest(
-                IEnumerable<TimeSpan> timeSpans)
+        public static IEnumerable<TimeSpan> OrderFromLongestToShortest(
+            IEnumerable<TimeSpan> timeSpans
+        )
         {
-            return from timeSpan in timeSpans
-                   orderby timeSpan descending
-                   select timeSpan;
+            return from timeSpan in timeSpans orderby timeSpan descending select timeSpan;
         }
 
         //Coding Exercise 2
-        public static IEnumerable<Point> OrderPoints(
-             IEnumerable<Point> points)
+        public static IEnumerable<Point> OrderPoints(IEnumerable<Point> points)
         {
-            return from point in points
-                   orderby point.X, point.Y
-                   select point;
+            return from point in points orderby point.X, point.Y select point;
         }
 
         //Refactoring challenge
-        public static IEnumerable<DateTime>
-            OrderDatesByDayOfWeek_Refactored(
-                IEnumerable<DateTime> dates)
+        public static IEnumerable<DateTime> OrderDatesByDayOfWeek_Refactored(
+            IEnumerable<DateTime> dates
+        )
         {
-            return from date in dates
-                   orderby date.DayOfWeek
-                   select date;
+            return from date in dates orderby date.DayOfWeek select date;
         }
 
         //do not modify this method
-        public static IEnumerable<DateTime>
-            OrderDatesByDayOfWeek(
-                IEnumerable<DateTime> dates)
+        public static IEnumerable<DateTime> OrderDatesByDayOfWeek(IEnumerable<DateTime> dates)
         {
             var result = dates.ToList();
-            result.Sort((left, right) =>
-            {
-                return left.DayOfWeek.CompareTo(right.DayOfWeek);
-            });
+            result.Sort(
+                (left, right) =>
+                {
+                    return left.DayOfWeek.CompareTo(right.DayOfWeek);
+                }
+            );
             return result;
         }
 
