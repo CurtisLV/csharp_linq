c1645b116196f8a2b1d70d0a82d96f14560ebf2b
CurtisLV <curtislv@gmail.com>
2023-04-01 17:58:37 +0300

    01.04. 6

--- ExercisesTests/Where_QuerySyntax_Exercise1_Tests.cs
+++ ExercisesTests/Where_QuerySyntax_Exercise1_Tests.cs
@@ -15,21 +15,25 @@ namespace ExercisesTests
         {
             var people = new List<Person>
             {
-                new Person("Geraldine Smith", new DateTime(1920, 12, 2) ),
-                new Person("John Smith", new DateTime(1950, 4, 8)) ,
-                new Person("Monica Smith", new DateTime(1954,11, 3)) ,
-                new Person("Jake Smith", new DateTime(1982, 8, 3) )
+                new Person("Geraldine Smith", new DateTime(1920, 12, 2)),
+                new Person("John Smith", new DateTime(1950, 4, 8)),
+                new Person("Monica Smith", new DateTime(1954, 11, 3)),
+                new Person("Jake Smith", new DateTime(1982, 8, 3))
             };
 
             var expectedResult = new List<Person>
             {
-                new Person("Monica Smith", new DateTime(1954, 11, 3) ),
-                new Person("Jake Smith", new DateTime(1982, 8, 3) )
+                new Person("Monica Smith", new DateTime(1954, 11, 3)),
+                new Person("Jake Smith", new DateTime(1982, 8, 3))
             };
 
             var result = WhereQuerySyntax.GetBornAfter(1950, people);
 
-            CollectionAssert.AreEqual(expectedResult, result, $"For input {EnumerableToString(people)} the result shall be {EnumerableToString(expectedResult)} but it was '{EnumerableToString(result)}'");
+            CollectionAssert.AreEqual(
+                expectedResult,
+                result,
+                $"For input {EnumerableToString(people)} the result shall be {EnumerableToString(expectedResult)} but it was '{EnumerableToString(result)}'"
+            );
         }
 
         [Test]
@@ -37,20 +41,24 @@ namespace ExercisesTests
         {
             var people = new List<Person>
             {
-                new Person("Geraldine Smith", new DateTime(1920, 12, 2) ),
-                new Person("John Smith", new DateTime(1950, 4, 8)) ,
-                new Person("Monica Smith", new DateTime(1954,11, 3)) ,
-                new Person("Jake Smith", new DateTime(1982, 8, 3) )
+                new Person("Geraldine Smith", new DateTime(1920, 12, 2)),
+                new Person("John Smith", new DateTime(1950, 4, 8)),
+                new Person("Monica Smith", new DateTime(1954, 11, 3)),
+                new Person("Jake Smith", new DateTime(1982, 8, 3))
             };
 
             var expectedResult = new List<Person>
             {
-                new Person("Jake Smith", new DateTime(1982, 8, 3) )
+                new Person("Jake Smith", new DateTime(1982, 8, 3))
             };
 
             var result = WhereQuerySyntax.GetBornAfter(1980, people);
 
-            CollectionAssert.AreEqual(expectedResult, result, $"For input {EnumerableToString(people)} the result shall be {EnumerableToString(expectedResult)} but it was '{EnumerableToString(result)}'");
+            CollectionAssert.AreEqual(
+                expectedResult,
+                result,
+                $"For input {EnumerableToString(people)} the result shall be {EnumerableToString(expectedResult)} but it was '{EnumerableToString(result)}'"
+            );
         }
     }
 }
