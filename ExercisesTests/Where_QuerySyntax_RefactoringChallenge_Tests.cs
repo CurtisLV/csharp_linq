4295ba6aa7b7ed85a4870b096d39aa0c6fe653a4
CurtisLV <curtislv@gmail.com>
2023-04-02 19:19:53 +0300

    02.04. 5

--- ExercisesTests/Where_QuerySyntax_RefactoringChallenge_Tests.cs
+++ ExercisesTests/Where_QuerySyntax_RefactoringChallenge_Tests.cs
@@ -10,89 +10,65 @@ namespace ExercisesTests
         [Test]
         public void FindWordsWithSubstringShallWorkCorrectly1()
         {
-            var words = new[] {
-                "racoon",
-                "raven",
-                "lion",
-                "crane",
-                "sardine"
-            };
+            var words = new[] { "racoon", "raven", "lion", "crane", "sardine" };
 
-            var expectedResult = new[] {
-                "crane",
-                "racoon",
-                "raven",
-            };
+            var expectedResult = new[] { "crane", "racoon", "raven", };
 
             var result = WhereQuerySyntax.FindWordsWithSubstring("ra", words);
 
-            CollectionAssert.AreEqual(expectedResult, result, $"For INPUT {EnumerableToString(words)} the RESULT shall be '{EnumerableToString(expectedResult)}' BUT IT WAS '{EnumerableToString(result)}'");
+            CollectionAssert.AreEqual(
+                expectedResult,
+                result,
+                $"For INPUT {EnumerableToString(words)} the RESULT shall be '{EnumerableToString(expectedResult)}' BUT IT WAS '{EnumerableToString(result)}'"
+            );
         }
 
         [Test]
         public void FindWordsWithSubstringShallWorkCorrectly2()
         {
-            var words = new[] {
-                "trout",
-                "heron",
-                "rhinoceros",
-                "duck",
-                "tiger"
-            };
+            var words = new[] { "trout", "heron", "rhinoceros", "duck", "tiger" };
 
-            var expectedResult = new[] {
-                "heron",
-                "rhinoceros",
-                "trout",
-            };
+            var expectedResult = new[] { "heron", "rhinoceros", "trout", };
 
             var result = WhereQuerySyntax.FindWordsWithSubstring("ro", words);
 
-            CollectionAssert.AreEqual(expectedResult, result, $"For INPUT {EnumerableToString(words)} the RESULT shall be '{EnumerableToString(expectedResult)}' BUT IT WAS '{EnumerableToString(result)}'");
+            CollectionAssert.AreEqual(
+                expectedResult,
+                result,
+                $"For INPUT {EnumerableToString(words)} the RESULT shall be '{EnumerableToString(expectedResult)}' BUT IT WAS '{EnumerableToString(result)}'"
+            );
         }
 
         [Test]
         public void FindWordsWithSubstringShallWorkCorrectly1_Refactored()
         {
-            var words = new[] {
-                "racoon",
-                "raven",
-                "lion",
-                "crane",
-                "sardine"
-            };
+            var words = new[] { "racoon", "raven", "lion", "crane", "sardine" };
 
-            var expectedResult = new[] {
-                "crane",
-                "racoon",
-                "raven",
-            };
+            var expectedResult = new[] { "crane", "racoon", "raven", };
 
             var result = WhereQuerySyntax.FindWordsWithSubstring_Refactored("ra", words);
 
-            CollectionAssert.AreEqual(expectedResult, result, $"For INPUT {EnumerableToString(words)} the RESULT shall be '{EnumerableToString(expectedResult)}' BUT IT WAS '{EnumerableToString(result)}'");
+            CollectionAssert.AreEqual(
+                expectedResult,
+                result,
+                $"For INPUT {EnumerableToString(words)} the RESULT shall be '{EnumerableToString(expectedResult)}' BUT IT WAS '{EnumerableToString(result)}'"
+            );
         }
 
         [Test]
         public void FindWordsWithSubstringShallWorkCorrectly2_Refactored()
         {
-            var words = new[] {
-                "trout",
-                "heron",
-                "rhinoceros",
-                "duck",
-                "tiger"
-            };
+            var words = new[] { "trout", "heron", "rhinoceros", "duck", "tiger" };
 
-            var expectedResult = new[] {
-                "heron",
-                "rhinoceros",
-                "trout",
-            };
+            var expectedResult = new[] { "heron", "rhinoceros", "trout", };
 
             var result = WhereQuerySyntax.FindWordsWithSubstring_Refactored("ro", words);
 
-            CollectionAssert.AreEqual(expectedResult, result, $"For INPUT {EnumerableToString(words)} the RESULT shall be '{EnumerableToString(expectedResult)}' BUT IT WAS '{EnumerableToString(result)}'");
+            CollectionAssert.AreEqual(
+                expectedResult,
+                result,
+                $"For INPUT {EnumerableToString(words)} the RESULT shall be '{EnumerableToString(expectedResult)}' BUT IT WAS '{EnumerableToString(result)}'"
+            );
         }
     }
 }
