25721120b6a82cc7eeea1c53bc33da6254027148
CurtisLV <curtislv@gmail.com>
2023-04-03 19:29:17 +0300

    03.04. 8

--- ExercisesTests/SelectQuerySyntax_Exercise2_Tests.cs
+++ ExercisesTests/SelectQuerySyntax_Exercise2_Tests.cs
@@ -13,23 +13,22 @@ namespace ExercisesTests
         {
             var houses = new[]
             {
-              new House("123 Old Road"),
-              new House("Pelican Town, 2 Willow Lane, Stardew Valley Borough"),
-              new House("Rocky Village, 22 Hill Road, Shadow Glen Borough"),
+                new House("123 Old Road"),
+                new House("Pelican Town, 2 Willow Lane, Stardew Valley Borough"),
+                new House("Rocky Village, 22 Hill Road, Shadow Glen Borough"),
             };
 
-            var expectedResult = new[] {
-              "123 Old Road",
-              "2 Willow Lane",
-              "22 Hill Road"
-            };
+            var expectedResult = new[] { "123 Old Road", "2 Willow Lane", "22 Hill Road" };
 
             var result = SelectQuerySyntax.GetShortAddresses(houses);
 
-            CollectionAssert.AreEqual(expectedResult, result, 
-                $"For INPUT {EnumerableToString(houses)} the RESULT shall be " +
-                $"'{EnumerableToString(expectedResult)}' BUT IT WAS " +
-                $"'{EnumerableToString(result)}'");
+            CollectionAssert.AreEqual(
+                expectedResult,
+                result,
+                $"For INPUT {EnumerableToString(houses)} the RESULT shall be "
+                    + $"'{EnumerableToString(expectedResult)}' BUT IT WAS "
+                    + $"'{EnumerableToString(result)}'"
+            );
         }
 
         [Test]
@@ -37,25 +36,30 @@ namespace ExercisesTests
         {
             var houses = new[]
             {
-              new House("Maple Town, 123 Old Road, South Slope Borough"),
-              new House("41 Beach Road"),
-              new House("Oak Village, 12 Forest Alley, North Lake Borough"),
-              new House("4 River Path"),
+                new House("Maple Town, 123 Old Road, South Slope Borough"),
+                new House("41 Beach Road"),
+                new House("Oak Village, 12 Forest Alley, North Lake Borough"),
+                new House("4 River Path"),
             };
 
-            var expectedResult = new[] {
-              "123 Old Road",
-              "41 Beach Road",
-              "12 Forest Alley",
-              "4 River Path",
+            var expectedResult = new[]
+            {
+                "123 Old Road",
+                "41 Beach Road",
+                "12 Forest Alley",
+                "4 River Path",
             };
 
             var result = SelectQuerySyntax.GetShortAddresses(houses);
 
-            CollectionAssert.AreEqual(expectedResult, result, $"For INPUT " +
-                $"{EnumerableToString(houses)} the RESULT shall be " +
-                $"'{EnumerableToString(expectedResult)}' BUT IT WAS " +
-                $"'{EnumerableToString(result)}'");
+            CollectionAssert.AreEqual(
+                expectedResult,
+                result,
+                $"For INPUT "
+                    + $"{EnumerableToString(houses)} the RESULT shall be "
+                    + $"'{EnumerableToString(expectedResult)}' BUT IT WAS "
+                    + $"'{EnumerableToString(result)}'"
+            );
         }
     }
 }
