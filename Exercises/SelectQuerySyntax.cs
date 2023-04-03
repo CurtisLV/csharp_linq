32579481a44060eab0d79b9a94b35e11edeba503
CurtisLV <curtislv@gmail.com>
2023-04-03 19:22:54 +0300

    03.04. 1

--- Exercises/SelectQuerySyntax.cs
+++ Exercises/SelectQuerySyntax.cs
@@ -8,19 +8,18 @@ namespace Exercises
     {
         //Coding Exercise 1
         /*
-         Using LINQ's query syntax, implement the GetAbsoluteValuesInfo method, 
-        which given a collection of numbers will return a collection of strings with 
+         Using LINQ's query syntax, implement the GetAbsoluteValuesInfo method,
+        which given a collection of numbers will return a collection of strings with
         information about those numbers' absolute values.
 
-        For example, for numbers {-7, 2, -3, 8} the result shall be a collection of 
+        For example, for numbers {-7, 2, -3, 8} the result shall be a collection of
         strings with the following values:
             "|-7|=7"
             "|2|=2"
             "|-3|=3"
             "|8|=8"
          */
-        public static IEnumerable<string>
-            GetAbsoluteValuesInfo(IEnumerable<int> numbers)
+        public static IEnumerable<string> GetAbsoluteValuesInfo(IEnumerable<int> numbers)
         {
             //  TODO your code goes here
             throw new NotImplementedException();
@@ -28,26 +27,26 @@ namespace Exercises
 
         //Coding Exercise 2
         /*
-        Imagine you are working on a system that collects information about houses in 
-        some area. Each house has an Address. Some of the houses have full addresses 
+        Imagine you are working on a system that collects information about houses in
+        some area. Each house has an Address. Some of the houses have full addresses
         defined, containing:
             *town name
             *road and the house number
-            *the borough 
+            *the borough
 
-        For example, "Maple Town, 123 Old Road, South Slope Borough". 
+        For example, "Maple Town, 123 Old Road, South Slope Borough".
         
-        On the other hand, some of the houses have only the road and the house number 
+        On the other hand, some of the houses have only the road and the house number
         specified in the address.
         
         For example, "41 Beach Road".
         
-        Using query syntax, implement the GetShortAddresses method, which given 
-        a collection of houses will return a collection of strings with only the road 
+        Using query syntax, implement the GetShortAddresses method, which given
+        a collection of houses will return a collection of strings with only the road
         and the house numbers of the houses.
         
         For example, for the following houses:
-            *new House("Maple Town, 123 Old Road, South Slope Borough"), 
+            *new House("Maple Town, 123 Old Road, South Slope Borough"),
             *new House("41 Beach Road")
             *new House("Oak Village, 12 Forest Alley, North Lake Borough"),
             *new House("4 River Path"),
@@ -58,7 +57,7 @@ namespace Exercises
             *"12 Forest Alley"
             *"4 River Path"
         
-        Assume all addresses are correct, and that the full addresses are always 
+        Assume all addresses are correct, and that the full addresses are always
         formatted like "{TOWN NAME}, {ROAD AND THE HOUSE NUMBER}, {BOROUGH}".
          */
         public static IEnumerable<string> GetShortAddresses(IEnumerable<House> houses)
@@ -69,8 +68,7 @@ namespace Exercises
 
         //Refactoring Challenge
         //TODO implement this method
-        public static string GetBestStudentInfo_Refactored(
-            IEnumerable<Student> students)
+        public static string GetBestStudentInfo_Refactored(IEnumerable<Student> students)
         {
             //TODO your code goes here
             throw new NotImplementedException();
@@ -89,9 +87,7 @@ namespace Exercises
             var maxMark = 0;
             foreach (var student in studentsAsList)
             {
-                var studentsMaxMark = student.Marks.Any() ?
-                    student.Marks.Max() :
-                    0;
+                var studentsMaxMark = student.Marks.Any() ? student.Marks.Max() : 0;
                 if (studentsMaxMark > maxMark)
                 {
                     maxMark = studentsMaxMark;
@@ -99,9 +95,9 @@ namespace Exercises
                 }
             }
 
-            return $"Best mark was earned by " +
-                   $"{studentWithMaxMark.Name}" +
-                   $" and it is {maxMark}";
+            return $"Best mark was earned by "
+                + $"{studentWithMaxMark.Name}"
+                + $" and it is {maxMark}";
         }
 
         public class House
