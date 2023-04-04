7dded8246a9d4214065dce39865c7ade48d0f73e
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-04-04 18:30:26 +0300

    04.04. 7

--- ExercisesSolutions/SelectManyQuerySyntax.cs
+++ ExercisesSolutions/SelectManyQuerySyntax.cs
@@ -7,45 +7,38 @@ namespace ExercisesSolutions
     public static class SelectManyQuerySyntax
     {
         //Coding Exercise 1
-        public static IEnumerable<string> GetStudentMarksInfo(
-            IEnumerable<Student> students)
+        public static IEnumerable<string> GetStudentMarksInfo(IEnumerable<Student> students)
         {
             return from student in students
-                   where student.Marks != null
-                   from mark in student.Marks
-                   orderby student.Name, mark
-                   select $"{student.Name}: {mark}";
+            where student.Marks != null
+            from mark in student.Marks
+            orderby student.Name, mark
+            select $"{student.Name}: {mark}";
         }
 
         //Coding Exercise 2
-        public static IEnumerable<Tuple<int, int>>
-            SelectAllCombinationsWithSumBelow100(
-                IEnumerable<int> numbers1,
-                IEnumerable<int> numbers2)
+        public static IEnumerable<Tuple<int, int>> SelectAllCombinationsWithSumBelow100(
+            IEnumerable<int> numbers1,
+            IEnumerable<int> numbers2
+        )
         {
             return from number1 in numbers1
-                   from number2 in numbers2
-                   where
-                    number1 + number2 < 100
-                   select new Tuple<int, int>(number1, number2);
+            from number2 in numbers2
+            where number1 + number2 < 100
+            select new Tuple<int, int>(number1, number2);
         }
 
         //Refactoring challenge
-        public static IEnumerable<string>
-            GetDivisorsInfo_Refactored(
-                IEnumerable<int> numbers)
+        public static IEnumerable<string> GetDivisorsInfo_Refactored(IEnumerable<int> numbers)
         {
             return from number in numbers
-                   from divisor in GetDivisors(number)
-                   orderby number, divisor
-                   select $"Number {number} is " +
-                   $"divisible by {divisor}";
+            from divisor in GetDivisors(number)
+            orderby number, divisor
+            select $"Number {number} is " + $"divisible by {divisor}";
         }
 
         //do not modify this method
-        public static IEnumerable<string>
-            GetDivisorsInfo(
-                IEnumerable<int> numbers)
+        public static IEnumerable<string> GetDivisorsInfo(IEnumerable<int> numbers)
         {
             var orderedNumbers = numbers.ToList();
             orderedNumbers.Sort();
@@ -56,9 +49,7 @@ namespace ExercisesSolutions
                 divisors.Sort();
                 foreach (var divisor in divisors)
                 {
-                    result.Add(
-                        $"Number {number} is " +
-                        $"divisible by {divisor}");
+                    result.Add($"Number {number} is " + $"divisible by {divisor}");
                 }
             }
             return result;
@@ -66,12 +57,9 @@ namespace ExercisesSolutions
 
         private static IEnumerable<int> GetDivisors(int number)
         {
-            return from potentialDivisor in
-                       Enumerable.Range(
-                           1,
-                           number)
-                   where number % potentialDivisor == 0
-                   select potentialDivisor;
+            return from potentialDivisor in Enumerable.Range(1, number)
+            where number % potentialDivisor == 0
+            select potentialDivisor;
         }
 
         public class Student
