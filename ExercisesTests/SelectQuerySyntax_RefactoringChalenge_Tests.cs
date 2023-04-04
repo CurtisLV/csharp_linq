6069c6cd2c3e64cf6aca58385ca13c699926d95a
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-04-04 18:18:30 +0300

    04.04. 2

--- ExercisesTests/SelectQuerySyntax_RefactoringChalenge_Tests.cs
+++ ExercisesTests/SelectQuerySyntax_RefactoringChalenge_Tests.cs
@@ -13,17 +13,21 @@ namespace ExercisesTests
         {
             var students = new[]
             {
-              new Student {Name = "Jane Smith", Marks = new int[0]},
-              new Student {Name = "John Smith", Marks = new [] {6}},
-              new Student {Name = "Martin Brown", Marks = new [] {4,5}},
-              new Student {Name = "Amelia Green", Marks = new [] {2, 4, 4}},
+                new Student { Name = "Jane Smith", Marks = new int[0] },
+                new Student { Name = "John Smith", Marks = new[] { 6 } },
+                new Student { Name = "Martin Brown", Marks = new[] { 4, 5 } },
+                new Student { Name = "Amelia Green", Marks = new[] { 2, 4, 4 } },
             };
 
             var expectedResult = "Best mark was earned by John Smith and it is 6";
 
             var result = SelectQuerySyntax.GetBestStudentInfo(students);
 
-            CollectionAssert.AreEqual(expectedResult, result, $"For INPUT {EnumerableToString(students)} the RESULT shall be '{expectedResult}' BUT IT WAS '{result}'");
+            CollectionAssert.AreEqual(
+                expectedResult,
+                result,
+                $"For INPUT {EnumerableToString(students)} the RESULT shall be '{expectedResult}' BUT IT WAS '{result}'"
+            );
         }
 
         [Test]
@@ -31,17 +35,21 @@ namespace ExercisesTests
         {
             var students = new[]
             {
-              new Student {Name = "Jane Smith", Marks = new int[0]},
-              new Student {Name = "John Smith", Marks = new [] {3,4,}},
-              new Student {Name = "Martin Brown", Marks = new [] {4,5}},
-              new Student {Name = "Amelia Green", Marks = new [] {2, 4, 4}},
+                new Student { Name = "Jane Smith", Marks = new int[0] },
+                new Student { Name = "John Smith", Marks = new[] { 3, 4, } },
+                new Student { Name = "Martin Brown", Marks = new[] { 4, 5 } },
+                new Student { Name = "Amelia Green", Marks = new[] { 2, 4, 4 } },
             };
 
             var expectedResult = "Best mark was earned by Martin Brown and it is 5";
 
             var result = SelectQuerySyntax.GetBestStudentInfo(students);
 
-            CollectionAssert.AreEqual(expectedResult, result, $"For INPUT {EnumerableToString(students)} the RESULT shall be '{expectedResult}' BUT IT WAS '{result}'");
+            CollectionAssert.AreEqual(
+                expectedResult,
+                result,
+                $"For INPUT {EnumerableToString(students)} the RESULT shall be '{expectedResult}' BUT IT WAS '{result}'"
+            );
         }
 
         [Test]
@@ -49,19 +57,23 @@ namespace ExercisesTests
         {
             var students = new[]
             {
-              new Student {Name = "Jane Smith", Marks = new int[0]},
-              new Student {Name = "John Smith", Marks = new [] {6}},
-              new Student {Name = "Martin Brown", Marks = new [] {4,5}},
-              new Student {Name = "Amelia Green", Marks = new [] {2, 4, 4}},
+                new Student { Name = "Jane Smith", Marks = new int[0] },
+                new Student { Name = "John Smith", Marks = new[] { 6 } },
+                new Student { Name = "Martin Brown", Marks = new[] { 4, 5 } },
+                new Student { Name = "Amelia Green", Marks = new[] { 2, 4, 4 } },
             };
 
             var expectedResult = "Best mark was earned by John Smith and it is 6";
 
             var result = SelectQuerySyntax.GetBestStudentInfo_Refactored(students);
 
-            CollectionAssert.AreEqual(expectedResult, result, $"For INPUT " +
-                $"{EnumerableToString(students)} the RESULT shall be '{expectedResult}' " +
-                $"BUT IT WAS '{result}'");
+            CollectionAssert.AreEqual(
+                expectedResult,
+                result,
+                $"For INPUT "
+                    + $"{EnumerableToString(students)} the RESULT shall be '{expectedResult}' "
+                    + $"BUT IT WAS '{result}'"
+            );
         }
 
         [Test]
@@ -69,47 +81,50 @@ namespace ExercisesTests
         {
             var students = new[]
             {
-              new Student {Name = "Jane Smith", Marks = new int[0]},
-              new Student {Name = "John Smith", Marks = new [] {3,4,}},
-              new Student {Name = "Martin Brown", Marks = new [] {4,5}},
-              new Student {Name = "Amelia Green", Marks = new [] {2, 4, 4}},
+                new Student { Name = "Jane Smith", Marks = new int[0] },
+                new Student { Name = "John Smith", Marks = new[] { 3, 4, } },
+                new Student { Name = "Martin Brown", Marks = new[] { 4, 5 } },
+                new Student { Name = "Amelia Green", Marks = new[] { 2, 4, 4 } },
             };
 
             var expectedResult = "Best mark was earned by Martin Brown and it is 5";
 
             var result = SelectQuerySyntax.GetBestStudentInfo_Refactored(students);
 
-            CollectionAssert.AreEqual(expectedResult, result, 
-                $"For INPUT {EnumerableToString(students)} the RESULT shall be " +
-                $"'{expectedResult}' BUT IT WAS '{result}'");
+            CollectionAssert.AreEqual(
+                expectedResult,
+                result,
+                $"For INPUT {EnumerableToString(students)} the RESULT shall be "
+                    + $"'{expectedResult}' BUT IT WAS '{result}'"
+            );
         }
 
         [Test]
         public void EmptyStudents()
         {
-            var students = new Student[]
-            {
-            };
-
+            var students = new Student[] { };
 
             var result = SelectQuerySyntax.GetBestStudentInfo(students);
 
-            CollectionAssert.AreEqual(null, result, $"For EMPTY INPUT the RESULT " +
-                $"shall be null BUT IT WAS '{result}'");
+            CollectionAssert.AreEqual(
+                null,
+                result,
+                $"For EMPTY INPUT the RESULT " + $"shall be null BUT IT WAS '{result}'"
+            );
         }
 
         [Test]
         public void EmptyStudents_Refactored()
         {
-            var students = new Student[]
-            {
-            };
-
+            var students = new Student[] { };
 
             var result = SelectQuerySyntax.GetBestStudentInfo_Refactored(students);
 
-            CollectionAssert.AreEqual(null, result, $"For EMPTY INPUT the RESULT shall " +
-                $"be null BUT IT WAS '{result}'");
+            CollectionAssert.AreEqual(
+                null,
+                result,
+                $"For EMPTY INPUT the RESULT shall " + $"be null BUT IT WAS '{result}'"
+            );
         }
     }
 }
