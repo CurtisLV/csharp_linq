30d01baf8e567529b4ebd1508fe4ed58cd6cde87
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-04-05 12:55:46 +0300

    05.04. 1

--- ExercisesTests/SelectMany_QuerySyntax_Exercise1_Tests.cs
+++ ExercisesTests/SelectMany_QuerySyntax_Exercise1_Tests.cs
@@ -13,25 +13,29 @@ namespace ExercisesTests
         {
             var students = new[]
             {
-              new Student {Name = "Jane Smith", Marks = new int[0]},
-              new Student {Name = "John Smith", Marks = new [] {6}},
-              new Student {Name = "Martin Brown", Marks = new [] {4,5}},
-              new Student {Name = "Amelia Green", Marks = new [] {2, 4, 4}},
+                new Student { Name = "Jane Smith", Marks = new int[0] },
+                new Student { Name = "John Smith", Marks = new[] { 6 } },
+                new Student { Name = "Martin Brown", Marks = new[] { 4, 5 } },
+                new Student { Name = "Amelia Green", Marks = new[] { 2, 4, 4 } },
             };
 
             var expectedResult = new[]
             {
-              "Amelia Green: 2",
-              "Amelia Green: 4",
-              "Amelia Green: 4",
-              "John Smith: 6",
-              "Martin Brown: 4",
-              "Martin Brown: 5",
+                "Amelia Green: 2",
+                "Amelia Green: 4",
+                "Amelia Green: 4",
+                "John Smith: 6",
+                "Martin Brown: 4",
+                "Martin Brown: 5",
             };
 
             var result = SelectManyQuerySyntax.GetStudentMarksInfo(students);
 
-            CollectionAssert.AreEqual(expectedResult, result, $"For INPUT {EnumerableToString(students)} the RESULT shall be '{EnumerableToString(expectedResult)}' BUT IT WAS '{EnumerableToString(result)}'");
+            CollectionAssert.AreEqual(
+                expectedResult,
+                result,
+                $"For INPUT {EnumerableToString(students)} the RESULT shall be '{EnumerableToString(expectedResult)}' BUT IT WAS '{EnumerableToString(result)}'"
+            );
         }
 
         [Test]
@@ -39,24 +43,28 @@ namespace ExercisesTests
         {
             var students = new[]
             {
-              new Student {Name = "Jane Smith", Marks = new [] {4,5}},
-              new Student {Name = "John Smith", Marks = new [] {6, 2}},
-              new Student {Name = "Martin Brown", Marks = null},
-              new Student {Name = "Amelia Green", Marks = new [] {2}},
+                new Student { Name = "Jane Smith", Marks = new[] { 4, 5 } },
+                new Student { Name = "John Smith", Marks = new[] { 6, 2 } },
+                new Student { Name = "Martin Brown", Marks = null },
+                new Student { Name = "Amelia Green", Marks = new[] { 2 } },
             };
 
             var expectedResult = new[]
             {
-              "Amelia Green: 2",
-              "Jane Smith: 4",
-              "Jane Smith: 5",
-              "John Smith: 2",
-              "John Smith: 6",
+                "Amelia Green: 2",
+                "Jane Smith: 4",
+                "Jane Smith: 5",
+                "John Smith: 2",
+                "John Smith: 6",
             };
 
             var result = SelectManyQuerySyntax.GetStudentMarksInfo(students);
 
-            CollectionAssert.AreEqual(expectedResult, result, $"For INPUT {EnumerableToString(students)} the RESULT shall be '{EnumerableToString(expectedResult)}' BUT IT WAS '{EnumerableToString(result)}'");
+            CollectionAssert.AreEqual(
+                expectedResult,
+                result,
+                $"For INPUT {EnumerableToString(students)} the RESULT shall be '{EnumerableToString(expectedResult)}' BUT IT WAS '{EnumerableToString(result)}'"
+            );
         }
     }
 }
