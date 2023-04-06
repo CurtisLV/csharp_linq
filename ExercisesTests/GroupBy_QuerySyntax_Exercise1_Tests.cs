7143b14e413c91401d6c31bfdcaacabff0fefc4d
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-04-06 16:06:15 +0300

    06.04. 2

--- ExercisesTests/GroupBy_QuerySyntax_Exercise1_Tests.cs
+++ ExercisesTests/GroupBy_QuerySyntax_Exercise1_Tests.cs
@@ -10,47 +10,44 @@ namespace ExercisesTests
         [Test]
         public void GroupByFirstDigitShallWorkCorrectly1()
         {
-            var numbers = new[]
-            {
-               1, 11, 44, 4, 62, 672, 921
-            };
+            var numbers = new[] { 1, 11, 44, 4, 62, 672, 921 };
 
             var expectedResult = new[]
             {
-              "FirstDigit: 1, numbers: 1,11",
-              "FirstDigit: 4, numbers: 44,4",
-              "FirstDigit: 6, numbers: 62,672",
-              "FirstDigit: 9, numbers: 921"
+                "FirstDigit: 1, numbers: 1,11",
+                "FirstDigit: 4, numbers: 44,4",
+                "FirstDigit: 6, numbers: 62,672",
+                "FirstDigit: 9, numbers: 921"
             };
 
             var result = GroupByQuerySyntax.GroupByFirstDigit(numbers);
 
-            CollectionAssert.AreEqual(expectedResult, result, 
-                $"For INPUT {EnumerableToString(numbers)} the RESULT shall be " +
-                $"'{EnumerableToString(expectedResult)}' BUT IT WAS " +
-                $"'{EnumerableToString(result)}'");
+            CollectionAssert.AreEqual(
+                expectedResult,
+                result,
+                $"For INPUT {EnumerableToString(numbers)} the RESULT shall be "
+                    + $"'{EnumerableToString(expectedResult)}' BUT IT WAS "
+                    + $"'{EnumerableToString(result)}'"
+            );
         }
 
         [Test]
         public void GroupByFirstDigitShallWorkCorrectly2()
         {
-            var numbers = new[]
-            {
-               9, 91, 9713, 994
-            };
+            var numbers = new[] { 9, 91, 9713, 994 };
 
-            var expectedResult = new[]
-            {
-              "FirstDigit: 9, numbers: 9,91,9713,994",
-            };
+            var expectedResult = new[] { "FirstDigit: 9, numbers: 9,91,9713,994", };
 
             var result = GroupByQuerySyntax.GroupByFirstDigit(numbers);
 
-            CollectionAssert.AreEqual(expectedResult, result, $"For INPUT " +
-                $"{EnumerableToString(numbers)} the RESULT shall be " +
-                $"'{EnumerableToString(expectedResult)}' BUT IT WAS " +
-                $"'{EnumerableToString(result)}'");
+            CollectionAssert.AreEqual(
+                expectedResult,
+                result,
+                $"For INPUT "
+                    + $"{EnumerableToString(numbers)} the RESULT shall be "
+                    + $"'{EnumerableToString(expectedResult)}' BUT IT WAS "
+                    + $"'{EnumerableToString(result)}'"
+            );
         }
-
     }
 }
