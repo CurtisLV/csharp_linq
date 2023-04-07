f7fcb565347f5d13f91270904ce5edb8f947b1bd
CurtisLV <curtislv@gmail.com>
2023-04-07 13:06:00 +0300

    07.04. 7

--- Exercises/GroupByQuerySyntax.cs
+++ Exercises/GroupByQuerySyntax.cs
@@ -64,7 +64,7 @@ namespace Exercises
             return from house in houses
             group house by house.OwnerId into housesByOwner
             where housesByOwner.Count() > 1
-            select $"Owner with ID {housesByOwner.Key} owns houses {housesByOwner}";
+            select $"Owner with ID {housesByOwner.Key} owns houses {string.Join(", ", housesByOwner)}";
         }
 
         //do not modify this method
