faefbb7058d239147319c42af169f177cf12a479
CurtisLV <curtislv@gmail.com>
2023-04-09 13:36:22 +0300

    09.04. 1-

--- ExercisesSolutions/JoinQuerySyntax.cs
+++ ExercisesSolutions/JoinQuerySyntax.cs
@@ -13,9 +13,9 @@ namespace ExercisesSolutions
         )
         {
             return from student in students
-            join house in houses on student.HouseId equals house.Id
-            orderby house.Name, student.Name
-            select $"{student.Name} from house {house.Name}";
+                join house in houses on student.HouseId equals house.Id
+                orderby house.Name, student.Name
+                select $"{student.Name} from house {house.Name}";
         }
 
         //Coding Exercise 2
@@ -25,11 +25,11 @@ namespace ExercisesSolutions
         )
         {
             return from house in houses
-            join student in students on house.Id equals student.HouseId into housesStudents
-            from houseStudent in housesStudents.DefaultIfEmpty()
-            let studentInfo = houseStudent == null ? "no students" : houseStudent.Name
-            orderby house.Name, studentInfo
-            select $"House name: {house.Name}, student: {studentInfo}";
+                join student in students on house.Id equals student.HouseId into housesStudents
+                from houseStudent in housesStudents.DefaultIfEmpty()
+                let studentInfo = houseStudent == null ? "no students" : houseStudent.Name
+                orderby house.Name, studentInfo
+                select $"House name: {house.Name}, student: {studentInfo}";
         }
 
         //Refactoring challenge
@@ -40,10 +40,10 @@ namespace ExercisesSolutions
         )
         {
             return from student in students
-            from subjectIds in student.SubjectsIds
-            join house in houses on student.HouseId equals house.Id
-            join subject in subjects on subjectIds equals subject.Id
-            select $"{student.Name} from house " + $"{house.Name} studies {subject.Name}";
+                from subjectIds in student.SubjectsIds
+                join house in houses on student.HouseId equals house.Id
+                join subject in subjects on subjectIds equals subject.Id
+                select $"{student.Name} from house " + $"{house.Name} studies {subject.Name}";
         }
 
         //do not modify this method
