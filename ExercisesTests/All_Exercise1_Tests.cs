ec0217cd1cdce59b7b2baa5257a515d2079d2889
CurtisLV <curtislv@gmail.com>
2023-04-11 19:17:13 +0300

    11.04. 5

--- ExercisesTests/All_Exercise1_Tests.cs
+++ ExercisesTests/All_Exercise1_Tests.cs
@@ -11,20 +11,24 @@ namespace ExercisesTests
         public void NotAllAreDivisibleBy10()
         {
             var input = new[] { 10, 11, 12 };
-            Assert.False(All.AreAllNumbersDivisibleBy10(input),
-                $"Test failed for numbers {EnumerableToString(input)}, " +
-                $"because the AreAllNumbersDivisibleBy10 method result was True," +
-                $" and 11 and 12 are not divisible by 10");
+            Assert.False(
+                All.AreAllNumbersDivisibleBy10(input),
+                $"Test failed for numbers {EnumerableToString(input)}, "
+                    + $"because the AreAllNumbersDivisibleBy10 method result was True,"
+                    + $" and 11 and 12 are not divisible by 10"
+            );
         }
 
         [Test]
         public void AllAreDivisibleBy10()
         {
             var input = new[] { 10, 20, 30 };
-            Assert.True(All.AreAllNumbersDivisibleBy10(input),
-                $"Test failed for numbers {EnumerableToString(input)}, " +
-                $"because the AreAllNumbersDivisibleBy10 method result was False, " +
-                $"and they are all divisible by 10");
+            Assert.True(
+                All.AreAllNumbersDivisibleBy10(input),
+                $"Test failed for numbers {EnumerableToString(input)}, "
+                    + $"because the AreAllNumbersDivisibleBy10 method result was False, "
+                    + $"and they are all divisible by 10"
+            );
         }
     }
 }
