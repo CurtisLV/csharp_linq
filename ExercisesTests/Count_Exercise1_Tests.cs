1703c87892b1cb572683e567b9e376e3b750544d
CurtisLV <curtislv@gmail.com>
2023-04-12 20:25:09 +0300

    12.04. 9

--- ExercisesTests/Count_Exercise1_Tests.cs
+++ ExercisesTests/Count_Exercise1_Tests.cs
@@ -12,9 +12,13 @@ namespace ExercisesTests
             var words = new[] { "aaaaabbbbb", "abc" };
             var wordsAsString = string.Join(", ", words);
             var result = Count.CountAllLongWords(words);
-            Assert.AreEqual(result, 0, $"Test failed, because there are 0 words " +
-                $"longer than 10 letters in the {wordsAsString} collection " +
-                $"(aaaaabbbbb is exactly 10 letters long)");
+            Assert.AreEqual(
+                result,
+                0,
+                $"Test failed, because there are 0 words "
+                    + $"longer than 10 letters in the {wordsAsString} collection "
+                    + $"(aaaaabbbbb is exactly 10 letters long)"
+            );
         }
 
         [Test]
@@ -23,9 +27,13 @@ namespace ExercisesTests
             var words = new[] { "aaaaabbbbbc", "abc" };
             var wordsAsString = string.Join(", ", words);
             var result = Count.CountAllLongWords(words);
-            Assert.AreEqual(result, 1, $"Test failed, because there is 1 word " +
-                $"longer than 10 letters in the {wordsAsString} collection " +
-                $"(aaaaabbbbbc is exactly 11 letters long)");
+            Assert.AreEqual(
+                result,
+                1,
+                $"Test failed, because there is 1 word "
+                    + $"longer than 10 letters in the {wordsAsString} collection "
+                    + $"(aaaaabbbbbc is exactly 11 letters long)"
+            );
         }
 
         [Test]
@@ -34,8 +42,12 @@ namespace ExercisesTests
             var words = new[] { "aaaaabbbbbc", "aaaaabbbbbccccc", "abc" };
             var wordsAsString = string.Join(", ", words);
             var result = Count.CountAllLongWords(words);
-            Assert.AreEqual(result, 2, $"Test failed, because there are 2 " +
-                $"words longer than 10 letters in the {wordsAsString} collection");
+            Assert.AreEqual(
+                result,
+                2,
+                $"Test failed, because there are 2 "
+                    + $"words longer than 10 letters in the {wordsAsString} collection"
+            );
         }
     }
 }
