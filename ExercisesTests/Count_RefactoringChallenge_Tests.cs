78dd8ca3f0461e84423f2c5fc751fff3c1edc9be
CurtisLV <curtislv@gmail.com>
2023-04-12 20:24:53 +0300

    12.04. 7

--- ExercisesTests/Count_RefactoringChallenge_Tests.cs
+++ ExercisesTests/Count_RefactoringChallenge_Tests.cs
@@ -10,61 +10,65 @@ namespace ExercisesTests
         [Test]
         public void SomeSequencesAreTooLong()
         {
-            var numberSequences = new[]
-            {
-                new [] {1,2,3},
-                new [] {1,2,3,4}
-            };
+            var numberSequences = new[] { new[] { 1, 2, 3 }, new[] { 1, 2, 3, 4 } };
             var numberSequencesAsString = string.Join(
-                ", ", numberSequences.Select(seq => seq.Count()));
+                ", ",
+                numberSequences.Select(seq => seq.Count())
+            );
             var result = Count.IsAnySequenceTooLong(numberSequences, 3);
-            Assert.True(result, $"The test failed because some of the sequences of " +
-                $"lengths {numberSequencesAsString} is longer than max count 3");
+            Assert.True(
+                result,
+                $"The test failed because some of the sequences of "
+                    + $"lengths {numberSequencesAsString} is longer than max count 3"
+            );
         }
 
         [Test]
         public void SomeSequencesAreTooLong_Refactored()
         {
-            var numberSequences = new[]
-            {
-                new [] {1,2,3},
-                new [] {1,2,3,4}
-            };
+            var numberSequences = new[] { new[] { 1, 2, 3 }, new[] { 1, 2, 3, 4 } };
             var numberSequencesAsString = string.Join(
-                ", ", numberSequences.Select(seq => seq.Count()));
+                ", ",
+                numberSequences.Select(seq => seq.Count())
+            );
             var result = Count.IsAnySequenceTooLong_Refactored(numberSequences, 3);
-            Assert.True(result, $"The test failed because some of the sequences of" +
-                $" lengths {numberSequencesAsString} is longer than max count 3");
+            Assert.True(
+                result,
+                $"The test failed because some of the sequences of"
+                    + $" lengths {numberSequencesAsString} is longer than max count 3"
+            );
         }
 
         [Test]
         public void NoneSequencesAreTooLong()
         {
-            var numberSequences = new[]
-            {
-                new [] {1,2,3},
-                new [] {1,2,3,4}
-            };
+            var numberSequences = new[] { new[] { 1, 2, 3 }, new[] { 1, 2, 3, 4 } };
             var numberSequencesAsString = string.Join(
-                ", ", numberSequences.Select(seq => seq.Count()));
+                ", ",
+                numberSequences.Select(seq => seq.Count())
+            );
             var result = Count.IsAnySequenceTooLong(numberSequences, 4);
-            Assert.False(result, $"The test failed because none of the sequences of " +
-                $"lengths {numberSequencesAsString} is longer than max count 4");
+            Assert.False(
+                result,
+                $"The test failed because none of the sequences of "
+                    + $"lengths {numberSequencesAsString} is longer than max count 4"
+            );
         }
 
         [Test]
         public void NoneSequencesAreTooLong_Refactored()
         {
-            var numberSequences = new[]
-            {
-                new [] {1,2,3},
-                new [] {1,2,3,4}
-            };
+            var numberSequences = new[] { new[] { 1, 2, 3 }, new[] { 1, 2, 3, 4 } };
             var numberSequencesAsString = string.Join(
-                ", ", numberSequences.Select(seq => seq.Count()));
+                ", ",
+                numberSequences.Select(seq => seq.Count())
+            );
             var result = Count.IsAnySequenceTooLong_Refactored(numberSequences, 4);
-            Assert.False(result, $"The test failed because none of the sequences of " +
-                $"lengths {numberSequencesAsString} is longer than max count 3");
+            Assert.False(
+                result,
+                $"The test failed because none of the sequences of "
+                    + $"lengths {numberSequencesAsString} is longer than max count 3"
+            );
         }
     }
 }
