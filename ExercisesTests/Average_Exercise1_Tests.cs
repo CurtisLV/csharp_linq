938778d401b6eb8dcafcc1323a269d47b0f95cd2
CurtisLV <curtislv@gmail.com>
2023-04-12 20:23:24 +0300

    12.04. 1

--- ExercisesTests/Average_Exercise1_Tests.cs
+++ ExercisesTests/Average_Exercise1_Tests.cs
@@ -42,23 +42,26 @@ namespace ExercisesTests
             {
                 MonthlySnowFallDataItems = new List<MonthlySnowFallData>
                 {
-
-                    new MonthlySnowFallData {SnowfallInCentimeters = 11},
-                    new MonthlySnowFallData {SnowfallInCentimeters = 22},
-                    new MonthlySnowFallData {SnowfallInCentimeters = 10},
-                    new MonthlySnowFallData {SnowfallInCentimeters = 9},
-                    new MonthlySnowFallData {SnowfallInCentimeters = 5},
-                    new MonthlySnowFallData {SnowfallInCentimeters = 0},
-                    new MonthlySnowFallData {SnowfallInCentimeters = 0},
-                    new MonthlySnowFallData {SnowfallInCentimeters = 1},
-                    new MonthlySnowFallData {SnowfallInCentimeters = 5},
-                    new MonthlySnowFallData {SnowfallInCentimeters = 18},
-                    new MonthlySnowFallData {SnowfallInCentimeters = 19},
-                    new MonthlySnowFallData {SnowfallInCentimeters = 32},
+                    new MonthlySnowFallData { SnowfallInCentimeters = 11 },
+                    new MonthlySnowFallData { SnowfallInCentimeters = 22 },
+                    new MonthlySnowFallData { SnowfallInCentimeters = 10 },
+                    new MonthlySnowFallData { SnowfallInCentimeters = 9 },
+                    new MonthlySnowFallData { SnowfallInCentimeters = 5 },
+                    new MonthlySnowFallData { SnowfallInCentimeters = 0 },
+                    new MonthlySnowFallData { SnowfallInCentimeters = 0 },
+                    new MonthlySnowFallData { SnowfallInCentimeters = 1 },
+                    new MonthlySnowFallData { SnowfallInCentimeters = 5 },
+                    new MonthlySnowFallData { SnowfallInCentimeters = 18 },
+                    new MonthlySnowFallData { SnowfallInCentimeters = 19 },
+                    new MonthlySnowFallData { SnowfallInCentimeters = 32 },
                 }
             };
             var result = Average.AverageSnowFall(snowFallData);
-            Assert.AreEqual(11, result, $"For snowfall of values 11, 22, 10, 9, 5, 0, 0, 1, 5, 18, 19, 32 the average should be 11");
+            Assert.AreEqual(
+                11,
+                result,
+                $"For snowfall of values 11, 22, 10, 9, 5, 0, 0, 1, 5, 18, 19, 32 the average should be 11"
+            );
         }
     }
 }
